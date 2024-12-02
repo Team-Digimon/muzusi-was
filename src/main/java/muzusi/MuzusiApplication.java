a707e293e476fd4522991f942b9b676cf2e2507f
DaeYoung0726 <pdy000726@naver.com>
2024-12-02 23:10:06 +0900

    chore: create project

---
+++ src/main/java/muzusi/MuzusiApplication.java
@@ -0,0 +1,13 @@
+package muzusi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzusiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzusiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/muzusi/MuzusiApplicationTests.java
@@ -0,0 +1,13 @@
+package muzusi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzusiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
