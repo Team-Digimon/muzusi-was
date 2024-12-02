4bd723f238d37ebd0282c0421ad2b73683b89baf
DaeYoung0726 <pdy000726@naver.com>
2024-12-03 00:18:36 +0900

    feat: PinnedStock Entity 작성

--- src/main/java/muzusi/domain/stock/entity/PinnedStock.java
+++ src/main/java/muzusi/domain/stock/entity/PinnedStock.java
@@ -1,2 +1,37 @@
-package muzusi.domain.stock.entity;public class PinnedStock {
+package muzusi.domain.stock.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import muzusi.domain.user.entity.User;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "pinned_stock")
+public class PinnedStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
+    @Builder
+    public PinnedStock(User user, Stock stock) {
+        this.user = user;
+        this.stock = stock;
+    }
 }
