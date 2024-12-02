437beac088c11d7a1c6c9e0a37c7601b2cc9b421
DaeYoung0726 <pdy000726@naver.com>
2024-12-03 00:18:21 +0900

    feat: Stock Entity 작성

--- src/main/java/muzusi/domain/stock/entity/Stock.java
+++ src/main/java/muzusi/domain/stock/entity/Stock.java
@@ -1,2 +1,32 @@
-package muzusi.domain.stock.entity;public class Stock {
+package muzusi.domain.stock.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "stock")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "stock_name", nullable = false)
+    private String stockName;
+
+    @Column(name = "stock_code", nullable = false)
+    private String stockCode;
+
+    @Builder
+    public Stock(String stockName, String stockCode) {
+        this.stockName = stockName;
+        this.stockCode = stockCode;
+    }
 }
