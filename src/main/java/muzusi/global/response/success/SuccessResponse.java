a577f445704d704646f04c765c7b2ba04d5d2d3e
hky035 <rldud035@naver.com>
2024-12-05 23:38:07 +0900

    feat: SuccessResponse 추가

---
+++ src/main/java/muzusi/global/response/success/SuccessResponse.java
@@ -0,0 +1,43 @@
+package muzusi.global.response.success;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+@Builder
+public record SuccessResponse<T>(
+        int code,
+        String message,
+        @JsonIgnore
+        T data
+){
+    public static SuccessResponse<?> ok() {
+        return SuccessResponse.builder()
+                .code(HttpStatus.OK.value())
+                .message("요청이 성공하였습니다.")
+                .build();
+    }
+
+    public static SuccessResponse<?> from(SuccessType success) {
+        return SuccessResponse.builder()
+                .code(success.getStatusCode())
+                .message(success.getMessage())
+                .build();
+    }
+
+    public static <T> SuccessResponse<?> from(T data) {
+        return SuccessResponse.builder()
+                .code(HttpStatus.OK.value())
+                .message("요청이 성공하였습니다.")
+                .data(data)
+                .build();
+    }
+
+    public static <T> SuccessResponse<?> of(SuccessType success, T data) {
+        return SuccessResponse.builder()
+                .code(success.getStatusCode())
+                .message(success.getMessage())
+                .data(data)
+                .build();
+    }
+}
