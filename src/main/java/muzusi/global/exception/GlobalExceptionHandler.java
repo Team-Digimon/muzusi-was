1f4ffa281c15bb2ac92cddb80dd6d6eca62ee966
hky035 <rldud035@naver.com>
2024-12-08 01:10:27 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/muzusi/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package muzusi.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import muzusi.global.response.error.ErrorResponse;
+import muzusi.global.response.error.type.BaseErrorType;
+import muzusi.global.response.error.type.CommonErrorType;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /* CustomException 예외 처리 */
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<?> handleCustomException(final CustomException e) {
+        BaseErrorType error = e.getErrorType();
+        log.error("[Error Occurred] {}\n<<Stack Trace 5 lines>>\n{}", e.getMessage(), getStackTrace(e));
+        return ResponseEntity.status(error.getStatus()).body(ErrorResponse.from(error));
+    }
+
+    /* Argument Validation 예외 처리 */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Map<String, String>> handleValidationException(final MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for(FieldError fieldError : e.getBindingResult().getFieldErrors() ){
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        log.error("[Error occured] {}",errors);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errors);
+    }
+
+    /* 일반 예외 처리 */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<?> handleException(final Exception e) {
+        log.error("[Error Occurred] {}\n<<Stack Trace 5 lines>>\n{}", e.getMessage(), getStackTrace(e));
+        BaseErrorType error = CommonErrorType.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(error.getStatus()).body(ErrorResponse.from(error));
+    }
+
+    /* StackTrace 5줄 출력 */
+    public String getStackTrace(Exception ex) {
+        StackTraceElement[] stackTrace = ex.getStackTrace();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Math.min(stackTrace.length, 5); i++) {
+            sb.append(stackTrace[i].toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
