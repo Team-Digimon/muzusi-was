110cf5ca30b4d9e8443857970669eaa732aa81e1
DaeYoung0726 <pdy000726@naver.com>
2024-12-11 03:19:42 +0900

    feat: UserDetails 구현

--- src/main/java/muzusi/security/auth/CustomUserDetails.java
+++ src/main/java/muzusi/security/auth/CustomUserDetails.java
@@ -1,2 +1,62 @@
-package muzusi.security.auth;public class CustomUserDetails {
+package muzusi.security.auth;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import muzusi.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private final Long userId;
+    private final String username;
+
+    public CustomUserDetails(Long userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public static CustomUserDetails from(User user) {
+        return new CustomUserDetails(user.getId(), user.getUsername());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
