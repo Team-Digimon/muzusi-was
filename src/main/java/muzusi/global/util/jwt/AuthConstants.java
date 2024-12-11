fea5dab58e4c265f7b1d5238987ef51106f85bc7
DaeYoung0726 <pdy000726@naver.com>
2024-12-12 03:02:22 +0900

    feat: Jwt 관련 상수 설정

--- src/main/java/muzusi/global/util/jwt/AuthConstants.java
+++ src/main/java/muzusi/global/util/jwt/AuthConstants.java
@@ -1,2 +1,14 @@
-package muzusi.global.util.jwt;public enum AuthConstants {
+package muzusi.global.util.jwt;
+
+import lombok.Getter;
+
+@Getter
+public enum AuthConstants {
+    AUTHORIZATION("Authorization"), TOKEN_TYPE("Bearer ");
+
+    private final String value;
+
+    AuthConstants(String value) {
+        this.value = value;
+    }
 }
