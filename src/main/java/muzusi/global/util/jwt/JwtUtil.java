efa261f1016993131f63f5c0605cae426b5b4583
DaeYoung0726 <pdy000726@naver.com>
2024-12-12 03:02:41 +0900

    feat: Jwt 파싱 util 메서드 구현

--- src/main/java/muzusi/global/util/jwt/JwtUtil.java
+++ src/main/java/muzusi/global/util/jwt/JwtUtil.java
@@ -1,2 +1,13 @@
-package muzusi.global.util.jwt;public class jwtUtil {
+package muzusi.global.util.jwt;
+
+public class JwtUtil {
+
+    /**
+     * Token TYPE("Bearer ") 파싱 메서드
+     * @param token : 토큰
+     * @return : TYPE 파싱 토큰
+     */
+    public static String resolveToken(String token) {
+        return token.substring(AuthConstants.TOKEN_TYPE.getValue().length()).trim();
+    }
 }
