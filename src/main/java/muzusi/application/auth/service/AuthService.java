1b32e9fdcb31130ab9c7ff036e25396cf7b1b8f2
DaeYoung0726 <pdy000726@naver.com>
2024-12-18 23:34:49 +0900

    feat: 로그인 로직 구현

--- src/main/java/muzusi/application/auth/service/AuthService.java
+++ src/main/java/muzusi/application/auth/service/AuthService.java
@@ -1,2 +1,22 @@
-package muzusi.application.auth.service;public class AuthService {
+package muzusi.application.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.application.auth.dto.LoginDto;
+import muzusi.application.auth.service.client.OAuthClient;
+import muzusi.application.auth.service.client.OAuthClientFactory;
+import muzusi.domain.user.type.OAuthPlatform;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final OAuthClientFactory oAuthClientFactory;
+
+    @Transactional
+    public void signIn(OAuthPlatform platform, String code) {
+        OAuthClient oAuthClient = oAuthClientFactory.getPlatformService(platform);
+
+        LoginDto loginDto = oAuthClient.login(code);
+    }
 }
