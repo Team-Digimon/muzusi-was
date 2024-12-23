9b8a6540ae21f2a57ec9ead95c244d6f348e32f8
DaeYoung0726 <pdy000726@naver.com>
2024-12-23 16:28:52 +0900

    feat: refreshtoken 상수 추가

--- src/main/java/muzusi/global/util/jwt/AuthConstants.java
+++ src/main/java/muzusi/global/util/jwt/AuthConstants.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AuthConstants {
-    AUTHORIZATION("Authorization"), TOKEN_TYPE("Bearer ");
+    AUTHORIZATION("Authorization"),
+    TOKEN_TYPE("Bearer "),
+    REFRESH_TOKEN_KEY("refreshToken");
 
     private final String value;
 }
