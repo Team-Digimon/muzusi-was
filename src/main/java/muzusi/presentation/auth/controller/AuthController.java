a864169eb98ee7b32a5a7f2f055e86177820c0fa
DaeYoung0726 <pdy000726@naver.com>
2024-12-23 16:30:01 +0900

    feat: 로그인 연결

--- src/main/java/muzusi/presentation/auth/controller/AuthController.java
+++ src/main/java/muzusi/presentation/auth/controller/AuthController.java
@@ -1,15 +1,23 @@
 package muzusi.presentation.auth.controller;
 
 import lombok.RequiredArgsConstructor;
+import muzusi.application.auth.dto.LoginDto;
 import muzusi.application.auth.dto.OAuthCodeDto;
 import muzusi.application.auth.service.AuthService;
 import muzusi.domain.user.type.OAuthPlatform;
+import muzusi.global.response.success.SuccessResponse;
+import muzusi.global.util.cookie.CookieUtil;
+import muzusi.global.util.jwt.AuthConstants;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,8 +28,23 @@ public class AuthController {
     @PostMapping("/sign-in/{platform}")
     public ResponseEntity<?> signIn(@RequestBody OAuthCodeDto oAuthCodeDto,
                                     @PathVariable OAuthPlatform platform) {
-        authService.signIn(platform, oAuthCodeDto.code());
+        LoginDto loginDto = authService.signIn(platform, oAuthCodeDto.code());
 
-        return ResponseEntity.ok("로그인 성공");
+        return createTokenRes(loginDto);
+    }
+
+    private ResponseEntity<?> createTokenRes(LoginDto loginDto) {
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("accessToken", loginDto.tokenDto().accessToken());
+        responseData.put("isRegistered", loginDto.isRegistered());
+
+        return ResponseEntity.ok()
+                .header("Set-Cookie",
+                        CookieUtil.createCookie(
+                                AuthConstants.REFRESH_TOKEN_KEY.getValue(),
+                                loginDto.tokenDto().refreshToken(),
+                                Duration.ofDays(7).toSeconds()
+                        ).toString())
+                .body(SuccessResponse.from(responseData));
     }
 }
