f17aaaf39a28a9c2e460520872648e7334fd5ff7
Kiyoung Heo <128910345+hky035@users.noreply.github.com>
2024-12-23 16:43:10 +0900

    Feature: WebSocket 및 Redis 초기 설정 (#20)
    * chore: WebSocket 및 Redis 패키지 설치
    
    * chore: redis 환경변수 추가
    
    * feat: RedisConfig 추가
    
    * feat: WebSocketConfig 추가

---
+++ src/main/java/muzusi/infrastructure/config/RedisConfig.java
@@ -0,0 +1,76 @@
+package muzusi.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer() );
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            ChannelTopic chart,
+            ChannelTopic koreaStockPulse,
+            ChannelTopic trade,
+            ChannelTopic stockRanking
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+
+        return container;
+    }
+
+    @Bean(name = "chart")
+    public ChannelTopic chart() {
+        return new ChannelTopic("chart");
+    }
+
+    @Bean(name = "koreaStockPulse")
+    public ChannelTopic koreaStockPulse() {
+        return new ChannelTopic("korea-stock-pulse");
+    }
+
+    @Bean(name = "trade")
+    public ChannelTopic trade(){
+        return new ChannelTopic("trade");
+    }
+
+    @Bean(name = "stockRanking")
+    public ChannelTopic ranking(){
+        return new ChannelTopic("stock-ranking");
+    }
+}
