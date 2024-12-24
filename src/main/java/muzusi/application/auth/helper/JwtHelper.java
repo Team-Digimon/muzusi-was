d39ded5adafae3b0210124b96917f1f70baff606
DaeYoung0726 <pdy000726@naver.com>
2024-12-24 17:45:25 +0900

    feat: 토큰 재발급 및 로그아웃 로직 구현

--- src/main/java/muzusi/application/auth/helper/JwtHelper.java
+++ src/main/java/muzusi/application/auth/helper/JwtHelper.java
@@ -1,15 +1,22 @@
 package muzusi.application.auth.helper;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import muzusi.application.auth.dto.TokenDto;
+import muzusi.application.auth.service.token.RefreshTokenService;
 import muzusi.domain.user.entity.User;
+import muzusi.global.exception.CustomException;
+import muzusi.global.response.error.type.CommonErrorType;
 import muzusi.global.security.jwt.JwtProvider;
+import muzusi.global.util.cookie.CookieUtil;
+import muzusi.global.util.jwt.AuthConstants;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
+@RequiredArgsConstructor
 public class JwtHelper {
     private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
 
     public TokenDto createToken(User user) {
         Long userId = user.getId();
@@ -18,6 +25,34 @@ public class JwtHelper {
         String accessToken = jwtProvider.generateAccessToken(username, userId);
         String refreshToken = jwtProvider.generateRefreshToken(username, userId);
 
+        refreshTokenService.saveRefreshToken(username, refreshToken);
+
         return TokenDto.of(accessToken, refreshToken);
+    }
+
+    public TokenDto reissueToken(String refreshToken) {
+        String username = jwtProvider.getUsername(refreshToken);
+
+        if (!refreshTokenService.existedRefreshToken(username))
+            throw new CustomException(CommonErrorType.REFRESH_TOKEN_NOT_FOUND);
+
+        Long userId = jwtProvider.getUserId(refreshToken);
+
+        String newAccessToken = jwtProvider.generateAccessToken(username, userId);
+        String newRefreshToken = jwtProvider.generateRefreshToken(username, userId);
+
+        refreshTokenService.saveRefreshToken(username, newRefreshToken);
+
+        return TokenDto.of(newAccessToken, newRefreshToken);
+    }
+
+    public void removeToken(String refreshToken, HttpServletResponse response) {
+        deleteRefreshToken(refreshToken, response);
+    }
+
+    private void deleteRefreshToken(String refreshToken, HttpServletResponse response) {
+        String username = jwtProvider.getUsername(refreshToken);
+        CookieUtil.deleteCookie(AuthConstants.REFRESH_TOKEN_KEY.getValue(), response);
+        refreshTokenService.deleteRefreshToken(username);
     }
 }
