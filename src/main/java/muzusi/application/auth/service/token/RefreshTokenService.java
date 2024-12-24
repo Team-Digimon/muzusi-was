00a8c49a7fdec4a1c22f6553e17751158d9ba890
DaeYoung0726 <pdy000726@naver.com>
2024-12-24 17:44:50 +0900

    feat: refrehsToken 로직 구현

---
+++ src/main/java/muzusi/application/auth/service/token/RefreshTokenService.java
@@ -0,0 +1,33 @@
+package muzusi.application.auth.service.token;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.global.redis.RedisService;
+import muzusi.global.util.jwt.AuthConstants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final RedisService redisService;
+
+    @Value("${token.refresh.in-redis}")
+    private long REDIS_REFRESH_EXPIRATION;
+
+    public void saveRefreshToken(String username, String refreshToken) {
+        String key = AuthConstants.REFRESH_TOKEN_KEY.getValue() + ":" + username;
+        redisService.set(key, refreshToken);
+        redisService.expire(key, REDIS_REFRESH_EXPIRATION);
+    }
+
+    public boolean existedRefreshToken(String username) {
+        String key = AuthConstants.REFRESH_TOKEN_KEY.getValue() + ":" + username;
+
+        return redisService.existed(key);
+    }
+
+    public void deleteRefreshToken(String username) {
+        String key = AuthConstants.REFRESH_TOKEN_KEY.getValue() + ":" + username;
+        redisService.del(key);
+    }
+}
