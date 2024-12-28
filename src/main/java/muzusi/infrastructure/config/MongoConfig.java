7efa785bf3c6e9eb2b7b9473b618f8905b63f534
Kiyoung Heo <128910345+hky035@users.noreply.github.com>
2024-12-29 00:54:15 +0900

    Fix: Transaction 미적용으로 인한 코드 삭제 (#28)
    * fix: Transaction 미적용으로 인한 코드 삭제

--- src/main/java/muzusi/infrastructure/config/MongoConfig.java
+++ src/main/java/muzusi/infrastructure/config/MongoConfig.java
@@ -6,12 +6,9 @@ import lombok.RequiredArgsConstructor;
 import muzusi.infrastructure.properties.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 @RequiredArgsConstructor
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
@@ -21,11 +18,6 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Bean
     public MongoClient mongoClient(){
         return MongoClients.create(mongoProperties.getConnectionUri());
-    }
-
-    @Bean
-    public MongoTransactionManager transactionManager(){
-        return new MongoTransactionManager(mongoDbFactory());
     }
 
     @Override
