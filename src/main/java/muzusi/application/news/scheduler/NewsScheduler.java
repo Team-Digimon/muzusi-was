6608133b600894b54ff89452ee75a5a8b466f7da
DaeYoung0726 <pdy000726@naver.com>
2024-12-30 20:24:21 +0900

    feat: 뉴스 동작 스케쥴러 구현

---
+++ src/main/java/muzusi/application/news/scheduler/NewsScheduler.java
@@ -0,0 +1,19 @@
+package muzusi.application.news.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.application.news.service.NewsService;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableScheduling
+@RequiredArgsConstructor
+public class NewsScheduler {
+    private final NewsService newsService;
+
+    @Scheduled(cron = "0 */30 7-22 * * *")
+    public void runDailyNewsProcessJob() {
+        newsService.searchNewsByKeyword();
+    }
+}
