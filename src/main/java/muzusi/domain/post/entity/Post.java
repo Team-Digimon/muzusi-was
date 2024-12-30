2f5e1538a1a28ec381c79adaeca8a3236f084f57
DaeYoung0726 <pdy000726@naver.com>
2024-12-30 20:21:40 +0900

    refact: Post 필드 변경

--- src/main/java/muzusi/domain/post/entity/Post.java
+++ src/main/java/muzusi/domain/post/entity/Post.java
@@ -20,18 +20,18 @@ public class Post {
 
     private String title;
 
-    private String publisher;
+    private String link;
 
-    private String image;
+    private String keyword;
 
-    @Column(name = "target_url")
-    private String targetUrl;
+    @Column(name = "pub_date")
+    private String pubDate;
 
     @Builder
-    public Post(String title, String publisher, String image, String targetUrl) {
+    public Post(String title, String link, String keyword, String pubDate) {
         this.title = title;
-        this.publisher = publisher;
-        this.image = image;
-        this.targetUrl = targetUrl;
+        this.link = link;
+        this.keyword = keyword;
+        this.pubDate = pubDate;
     }
 }
