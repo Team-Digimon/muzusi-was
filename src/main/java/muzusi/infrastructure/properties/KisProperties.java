e33f82d67a965f3aaa5c98a40446ec0dbb352851
hky035 <rldud035@naver.com>
2025-01-01 22:31:56 +0900

    feat: KisProperties 작성

---
+++ src/main/java/muzusi/infrastructure/properties/KisProperties.java
@@ -0,0 +1,20 @@
+package muzusi.infrastructure.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import muzusi.infrastructure.kis.KisUrlConstant;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "kis")
+@Getter @Setter
+public class KisProperties {
+    private String domain;
+    private String appKey;
+    private String appSecret;
+
+    public String getUrl(KisUrlConstant kisUrlConstant){
+        return domain + kisUrlConstant.getUrl();
+    }
+}
