9cb6e7e7d8be6e9f2b7495d354b446a63ce1ef98
DaeYoung0726 <pdy000726@naver.com>
2025-01-02 00:21:11 +0900

    feat: 뉴스 불러오기 구현

---
+++ src/main/java/muzusi/application/news/dto/NewsInfoDto.java
@@ -0,0 +1,21 @@
+package muzusi.application.news.dto;
+
+import muzusi.domain.post.entity.News;
+
+import java.time.LocalDateTime;
+
+public record NewsInfoDto(
+        String title,
+        String link,
+        String keyword,
+        LocalDateTime pubDate
+) {
+    public static NewsInfoDto from(News news) {
+        return new NewsInfoDto(
+                news.getTitle(),
+                news.getLink(),
+                news.getKeyword(),
+                news.getPubDate()
+        );
+    }
+}
