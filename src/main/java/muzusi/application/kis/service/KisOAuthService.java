b9530ba4e1bfd282923dadf2cd501710e67db009
hky035 <rldud035@naver.com>
2025-01-09 18:28:14 +0900

    fix: 접근토큰 및 웹소켓접속키 dto 클래스 생성으로 인한 코드 변경

--- src/main/java/muzusi/application/kis/service/KisOAuthService.java
+++ src/main/java/muzusi/application/kis/service/KisOAuthService.java
@@ -3,6 +3,7 @@ package muzusi.application.kis.service;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import muzusi.application.kis.dto.KisAuthDto;
 import muzusi.global.redis.RedisService;
 import muzusi.infrastructure.kis.KisOAuthClient;
 import muzusi.infrastructure.kis.KisConstant;
@@ -18,6 +19,9 @@ public class KisOAuthService {
 
     @PostConstruct
     public void issueWebSocketKey() {
+        // 0109 테스트용 수정
+//        saveAccessToken();
+
         this.saveWebSocketKey();
     }
 
@@ -26,9 +30,11 @@ public class KisOAuthService {
      * 접근토큰 발급 오류 발생 시, DB 데이터 갱신 미실시
      */
     public void saveAccessToken() {
-        String accessToken = kisOAuthClient.getAccessToken();
+        KisAuthDto.AccessToken accessToken = KisAuthDto.AccessToken.builder()
+                .value(kisOAuthClient.getAccessToken())
+                .build();
 
-        if (accessToken != null)
+        if (accessToken.getValue() != null)
             redisService.set(KisConstant.ACCESS_TOKEN_PREFIX.getValue(), accessToken, Duration.ofDays(1));
     }
 
@@ -37,9 +43,11 @@ public class KisOAuthService {
      * 웹소켓 접속키 발급 오류 발생 시, DB 데이터 갱신 미실시
      */
     public void saveWebSocketKey() {
-        String webSocketKey = kisOAuthClient.getWebSocketKey();
+        KisAuthDto.WebSocketKey webSocketKey = KisAuthDto.WebSocketKey.builder()
+                .value(kisOAuthClient.getWebSocketKey())
+                .build();
 
-        if (webSocketKey != null)
+        if (webSocketKey.getValue() != null)
             redisService.set(KisConstant.WEBSOCKET_KEY_PREFIX.getValue(), webSocketKey, Duration.ofDays(365));
     }
 }
