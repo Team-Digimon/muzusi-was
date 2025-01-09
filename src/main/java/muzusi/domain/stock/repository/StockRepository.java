88df1c648771439b716e3cbfc3553588fd64474a
hky035 <rldud035@naver.com>
2025-01-09 18:25:29 +0900

    feat: 주식 조회 메서드 추가

---
+++ src/main/java/muzusi/domain/stock/repository/StockRepository.java
@@ -0,0 +1,10 @@
+package muzusi.domain.stock.repository;
+
+import muzusi.domain.stock.entity.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StockRepository extends JpaRepository<Stock, Long> {
+    Optional<Stock> findByStockName(String stockName);
+}
