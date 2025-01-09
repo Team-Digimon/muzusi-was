1ffd5590805d4b17d8b7d6505d0ab0dbb817e73f
hky035 <rldud035@naver.com>
2025-01-09 18:27:29 +0900

    fix: 한국투자증권 접근토큰 및 웹소켓 접속키 직렬화 에러 수정을 위한 dto 클래스 생성

---
+++ src/main/java/muzusi/application/kis/dto/KisAuthDto.java
@@ -0,0 +1,30 @@
+package muzusi.application.kis.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class KisAuthDto {
+
+    @NoArgsConstructor
+    @Getter
+    public static class AccessToken {
+        private String value;
+
+        @Builder
+        public AccessToken(String value) {
+            this.value = value;
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class WebSocketKey {
+        private String value;
+
+        @Builder
+        public  WebSocketKey(String value) {
+            this.value = value;
+        }
+    }
+}
