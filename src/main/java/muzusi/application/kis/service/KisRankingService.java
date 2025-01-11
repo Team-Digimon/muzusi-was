a2076232fd63ce072c58be28c3bfc2d25c113769
hky035 <rldud035@naver.com>
2025-01-11 18:26:24 +0900

    refact: RankStockDto 변경에 따른 코드 변경

--- src/main/java/muzusi/application/kis/service/KisRankingService.java
+++ src/main/java/muzusi/application/kis/service/KisRankingService.java
@@ -1,9 +1,7 @@
 package muzusi.application.kis.service;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import muzusi.domain.stock.entity.RankStock;
+import muzusi.application.stock.dto.RankStockDto;
 import muzusi.global.redis.RedisService;
 import muzusi.infrastructure.kis.KisConstant;
 import muzusi.infrastructure.kis.KisRankingClient;
@@ -11,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KisRankingService {
@@ -19,10 +16,10 @@ public class KisRankingService {
     private final RedisService redisService;
 
     public void saveVolumeRank() {
-        List<RankStock> rankStocks = kisRankingClient.getVolumeRank();
+        List<RankStockDto> rankStockDtos = kisRankingClient.getVolumeRank();
 
-        for (RankStock rankStock : rankStocks) {
-            redisService.setList(KisConstant.VOLUME_RANK_PREFIX.getValue(), rankStock);
+        for (RankStockDto rankStockDto : rankStockDtos) {
+            redisService.setList(KisConstant.VOLUME_RANK_PREFIX.getValue(), rankStockDto);
         }
     }
 }
--- src/main/java/muzusi/infrastructure/kis/KisRankingClient.java
+++ src/main/java/muzusi/infrastructure/kis/KisRankingClient.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import muzusi.application.kis.dto.KisAuthDto;
+import muzusi.application.stock.dto.RankStockDto;
 import muzusi.application.stock.service.StockService;
-import muzusi.domain.stock.entity.RankStock;
 import muzusi.global.redis.RedisService;
 import muzusi.infrastructure.properties.KisProperties;
 import org.springframework.http.*;
@@ -27,7 +27,7 @@ public class KisRankingClient {
     private final RedisService redisService;
     private final StockService stockService;
 
-    public List<RankStock> getVolumeRank() {
+    public List<RankStockDto> getVolumeRank() {
         HttpHeaders headers = getHttpHeaders();
 
         String uri = UriComponentsBuilder.fromUriString(kisProperties.getUrl(KisUrlConstant.VOLUME_RANK))
@@ -80,31 +80,31 @@ public class KisRankingClient {
         return headers;
     }
 
-    private List<RankStock> getRankStocks(JsonNode node) {
-        List<RankStock> rankStocks = new ArrayList<>();
+    private List<RankStockDto> getRankStocks(JsonNode node) {
+        List<RankStockDto> rankStockDtos = new ArrayList<>();
 
         for(JsonNode n : node) {
             Optional<Long> stockId = stockService.readByStockName(n.get("hts_kor_isnm").asText())
                             .map(stock -> stock.getId());
 
             if(stockId.isPresent()) {
-                RankStock rankStock = RankStock.builder()
+                RankStockDto rankStockDto = RankStockDto.builder()
                         .id(stockId.get())
                         .name(n.get("hts_kor_isnm").asText())
                         .code(n.get("mksc_shrn_iscd").asInt())
                         .rank(n.get("data_rank").asInt())
                         .price(n.get("stck_prpr").asLong())
-                        .prdy_vrss(n.get("prdy_vrss").asLong())
-                        .prdy_ctrt(n.get("prdy_ctrt").asInt())
-                        .avrg_vol(n.get("avrg_vol").asLong())
-                        .acml_tr_pbmn(n.get("acml_tr_pbmn").asLong())
+                        .prdyVrss(n.get("prdy_vrss").asLong())
+                        .prdyCtrt(n.get("prdy_ctrt").asInt())
+                        .avrgVol(n.get("avrg_vol").asLong())
+                        .acmlTrPbmn(n.get("acml_tr_pbmn").asLong())
                         .build();
 
-                rankStocks.add(rankStock);
+                rankStockDtos.add(rankStockDto);
             } else {
                 log.error("[KIS ERROR] " + n.get("hts_kor_isnm").asText() + "는 데이터베이스 상 존재하지 않는 주식입니다.");
             }
         }
-        return rankStocks;
+        return rankStockDtos;
     }
 }
