0d6359e0251f88b1a478fa6c09c1464fdbefabb7
DaeYoung0726 <pdy000726@naver.com>
2025-01-13 00:50:22 +0900

    feat: Account 생성시간 필드 추가

--- src/main/java/muzusi/domain/account/entity/Account.java
+++ src/main/java/muzusi/domain/account/entity/Account.java
@@ -2,6 +2,7 @@ package muzusi.domain.account.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,9 +14,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import muzusi.domain.user.entity.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity(name = "account")
 public class Account {
     @Id
@@ -26,6 +32,10 @@ public class Account {
 
     @Column(name = "rate_of_return")
     private double rateOfReturn;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
