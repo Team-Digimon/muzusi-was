78d0cf185cfd81747582827727eef9646e3761e6
DaeYoung0726 <pdy000726@naver.com>
2025-01-13 00:24:20 +0900

    feat: userId로 계좌 불러오기 작성

--- src/main/java/muzusi/application/account/service/UserAccountService.java
+++ src/main/java/muzusi/application/account/service/UserAccountService.java
@@ -1,6 +1,8 @@
 package muzusi.application.account.service;
 
 import lombok.RequiredArgsConstructor;
+import muzusi.application.account.dto.AccountInfoDto;
+import muzusi.domain.account.service.AccountService;
 import muzusi.domain.user.entity.User;
 import muzusi.domain.user.exception.UserErrorType;
 import muzusi.domain.user.service.UserService;
@@ -8,10 +10,13 @@ import muzusi.global.exception.CustomException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserAccountService {
     private final UserService userService;
+    private final AccountService accountService;
     private final AccountManagementService accountManagementService;
 
     /**
@@ -26,5 +31,18 @@ public class UserAccountService {
 
         foundUser.incrementAttemptCount();
         accountManagementService.createAndLinkAccount(foundUser);
+    }
+
+    /**
+     * 사용자의 계좌들을 불러오는 메서드
+     *
+     * @param userId : 사용자의 pk값
+     * @return : 사용자의 계좌 정보 list
+     */
+    @Transactional(readOnly = true)
+    public List<AccountInfoDto> getAllAccounts(Long userId) {
+        return accountService.readAllByUserId(userId)
+                .stream().map(AccountInfoDto::fromEntity)
+                .toList();
     }
 }
