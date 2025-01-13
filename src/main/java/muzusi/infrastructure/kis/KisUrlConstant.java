0b19182ecc606c4a99771dc3af226c62d85d91c9
hky035 <rldud035@naver.com>
2025-01-14 00:40:18 +0900

    chore: 등락률 API uri 엔드포인트 네이밍 수정

--- src/main/java/muzusi/infrastructure/kis/KisUrlConstant.java
+++ src/main/java/muzusi/infrastructure/kis/KisUrlConstant.java
@@ -9,7 +9,7 @@ public enum KisUrlConstant {
     ACCESS_TOKEN_ISSUE("/oauth2/tokenP"),
     WEBSOCKET_KEY_ISSUE("/oauth2/Approval"),
     VOLUME_RANK("/uapi/domestic-stock/v1/quotations/volume-rank"),
-    FLUCTUATION("/uapi/domestic-stock/v1/ranking/fluctuation"),
+    FLUCTUATION_RANK("/uapi/domestic-stock/v1/ranking/fluctuation"),
     INQUIRE_PRICE("/uapi/domestic-stock/v1/quotations/inquire-price"),
     ;
 
