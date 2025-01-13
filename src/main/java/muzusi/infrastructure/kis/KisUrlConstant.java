050abd32abac799c68850b68b0a8bd26db3cb045
hky035 <rldud035@naver.com>
2025-01-14 00:17:08 +0900

    feat: 한국투자증권 현재가 조회 API URI 추가

--- src/main/java/muzusi/infrastructure/kis/KisUrlConstant.java
+++ src/main/java/muzusi/infrastructure/kis/KisUrlConstant.java
@@ -10,6 +10,7 @@ public enum KisUrlConstant {
     WEBSOCKET_KEY_ISSUE("/oauth2/Approval"),
     VOLUME_RANK("/uapi/domestic-stock/v1/quotations/volume-rank"),
     FLUCTUATION("/uapi/domestic-stock/v1/ranking/fluctuation"),
+    INQUIRE_PRICE("/uapi/domestic-stock/v1/quotations/inquire-price"),
     ;
 
     private final String url;
