665f9db0922f074553a72f5d5122feb84409156f
DaeYoung0726 <pdy000726@naver.com>
2025-01-13 20:10:13 +0900

    docs: 매수/매도 swagger 작성

--- src/main/java/muzusi/presentation/trade/controller/TradeController.java
+++ src/main/java/muzusi/presentation/trade/controller/TradeController.java
@@ -6,6 +6,7 @@ import muzusi.application.trade.dto.TradeReqDto;
 import muzusi.application.trade.service.StockTradeService;
 import muzusi.global.response.success.SuccessResponse;
 import muzusi.global.security.auth.CustomUserDetails;
+import muzusi.presentation.trade.api.TradeApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stocks")
 @RequiredArgsConstructor
-public class TradeController {
+public class TradeController implements TradeApi {
     private final StockTradeService stockTradeService;
 
+    @Override
     @PostMapping
     public ResponseEntity<?> tradeStock(@AuthenticationPrincipal CustomUserDetails userDetails,
                                         @Valid @RequestBody TradeReqDto tradeReqDto) {
