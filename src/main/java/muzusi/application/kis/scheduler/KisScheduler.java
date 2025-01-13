0049a788b4b6f24595730295d90c2167ebdc2ad8
hky035 <rldud035@naver.com>
2025-01-14 00:25:11 +0900

    feat: 등락률 순위 저장 스케줄링 적용

--- src/main/java/muzusi/application/kis/scheduler/KisScheduler.java
+++ src/main/java/muzusi/application/kis/scheduler/KisScheduler.java
@@ -2,6 +2,7 @@ package muzusi.application.kis.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import muzusi.application.kis.service.KisOAuthService;
+import muzusi.application.kis.service.KisRankingService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KisScheduler {
     private final KisOAuthService kisOAuthService;
+    private final KisRankingService kisRankingService;
 
     @Scheduled(cron = "0 0 7 * * ?")
     public void runIssueAccessTokenJob() {
@@ -18,5 +20,17 @@ public class KisScheduler {
     @Scheduled(cron = "0 0 0 1 1 ?")
     public void runIssueWebSocketKeyJob() {
         kisOAuthService.saveWebSocketKey();
+    }
+
+    @Scheduled(cron = "0 0/10 9-14 * * 1-5")
+    public void runRankingJob() {
+        kisRankingService.saveVolumeRank();
+        kisRankingService.saveFluctuationRank();
+    }
+
+    @Scheduled(cron = "0 10,20,30 15 * * 1-5")
+    public void runRankingJobAt3PM() {
+        kisRankingService.saveVolumeRank();
+        kisRankingService.saveFluctuationRank();
     }
 }
