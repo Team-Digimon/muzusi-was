8833780c915170ac6884349622c979df6023d03a
hky035 <rldud035@naver.com>
2025-01-15 20:06:52 +0900

    feat: 거래대금 및 주식 고유식별번호 미저장으로 인한 필드 및 메서드 삭제

--- src/main/java/muzusi/application/stock/dto/RankStockDto.java
+++ src/main/java/muzusi/application/stock/dto/RankStockDto.java
@@ -6,8 +6,6 @@ import lombok.Builder;
 public record RankStockDto(
         /* 종목 이름 */
         String name,
-        /* 고유 식별 번호*/
-        Long id,
         /* 종목 코드 */
         int code,
         /* 랭킹 */
@@ -19,21 +17,6 @@ public record RankStockDto(
         /* 등락률 */
         int prdyCtrt,
         /* 거래량 */
-        Long avrgVol,
-        /* 누적 거래 대금 */
-        Long acmlTrPbmn
+        Long avrgVol
 ) {
-    public static RankStockDto of(FluctuationRankStockDto fluctuationRankStockDto, String tradingValue) {
-        return RankStockDto.builder()
-                .id(fluctuationRankStockDto.id())
-                .name(fluctuationRankStockDto.name())
-                .code(fluctuationRankStockDto.code())
-                .rank(fluctuationRankStockDto.rank())
-                .price(fluctuationRankStockDto.price())
-                .prdyVrss(fluctuationRankStockDto.prdyVrss())
-                .prdyCtrt(fluctuationRankStockDto.prdyCtrt())
-                .avrgVol(fluctuationRankStockDto.avrgVol())
-                .acmlTrPbmn(Long.valueOf(tradingValue))
-                .build();
-    }
 }
