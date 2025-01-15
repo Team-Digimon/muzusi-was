e4e542f8a4eeb000050a7695b09f07baa6c97c90
DaeYoung0726 <pdy000726@naver.com>
2025-01-16 01:54:23 +0900

    feat: 몽고DB 시간 자동 지정을 위한 Auditing 설정

--- src/main/java/muzusi/infrastructure/config/MongoConfig.java
+++ src/main/java/muzusi/infrastructure/config/MongoConfig.java
@@ -7,9 +7,11 @@ import muzusi.infrastructure.properties.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMongoAuditing
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
     private final MongoProperties mongoProperties;
