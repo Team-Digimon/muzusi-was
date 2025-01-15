aea8ce3cc27eb9b817c90bdf20be7dda909e0845
DaeYoung0726 <pdy000726@naver.com>
2025-01-16 01:53:39 +0900

    feat: 주식 거래 예약 내역 몽고DB 컬렉션 설정

---
+++ src/main/java/muzusi/domain/trade/entity/TradeReservation.java
@@ -0,0 +1,42 @@
+package muzusi.domain.trade.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import muzusi.domain.trade.type.TradeType;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "trade_reservation")
+public class TradeReservation {
+    @Id
+    private String id;
+
+    private Long userId;
+
+    private Long inputPrice;
+
+    private Integer stockCount;
+
+    private String stockCode;
+
+    private TradeType tradeType;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Builder
+    public TradeReservation(Long userId, Long inputPrice, Integer stockCount, String stockCode, TradeType tradeType) {
+        this.userId = userId;
+        this.inputPrice = inputPrice;
+        this.stockCount = stockCount;
+        this.stockCode = stockCode;
+        this.tradeType = tradeType;
+    }
+}
