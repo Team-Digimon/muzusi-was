8804bc4aac77d7c043ffbd6757c035abb0efd467
DaeYoung0726 <pdy000726@naver.com>
2025-01-16 02:01:16 +0900

    feat: 주식 거래 분기 처리 구현

--- src/main/java/muzusi/application/trade/service/StockTradeService.java
+++ src/main/java/muzusi/application/trade/service/StockTradeService.java
@@ -1,48 +1,53 @@
 package muzusi.application.trade.service;
 
 import lombok.RequiredArgsConstructor;
-import muzusi.application.stock.service.StockService;
 import muzusi.application.trade.dto.TradeReqDto;
-import muzusi.domain.account.entity.Account;
-import muzusi.domain.account.exception.AccountErrorType;
-import muzusi.domain.account.service.AccountService;
-import muzusi.domain.stock.entity.Stock;
-import muzusi.domain.stock.exception.StockErrorType;
-import muzusi.domain.trade.entity.Trade;
-import muzusi.domain.trade.service.TradeService;
-import muzusi.global.exception.CustomException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class StockTradeService {
-    private final TradeService tradeService;
-    private final StockService stockService;
-    private final AccountService accountService;
+    private final StockTradeExecutor stockTradeExecutor;
+    private final TradeReservationHandler tradeReservationHandler;
 
     /**
-     * 주식 매수, 매도 메서드
+     * 주식 거래의 상태(매수/매도 가능 및 예약 처리)를 확인하는 메서드
      *
      * @param userId : 사용자 pk값
      * @param tradeReqDto : trade 정보 dto
      */
-    @Transactional
     public void tradeStock(Long userId, TradeReqDto tradeReqDto) {
-        Account account = accountService.readByUserId(userId)
-                .orElseThrow(() -> new CustomException(AccountErrorType.NOT_FOUND));
-
-        Stock stock = stockService.readByStockCode(tradeReqDto.stockCode())
-                .orElseThrow(() -> new CustomException(StockErrorType.NOT_FOUND));
-
-        tradeService.save(
-                Trade.builder()
-                        .stockPrice(tradeReqDto.stockPrice())
-                        .stockCount(tradeReqDto.stockCount())
-                        .tradeType(tradeReqDto.tradeType())
-                        .stock(stock)
-                        .account(account)
-                        .build()
-        );
+        switch (tradeReqDto.tradeType()) {
+            case BUY -> processBuyTrade(userId, tradeReqDto);
+            case SELL -> processSellTrade(userId, tradeReqDto);
+            default -> throw new IllegalArgumentException("잘못된 거래 유형입니다.");
+        }
     }
+
+    /**
+     * 주식을 사는 메서드.
+     * 사용자 입력 값 >= 주식 가격 : 주식 거래 성사
+     * 사용자 입력 값 < 주식 가격 : 주식 거래 예약
+     */
+    private void processBuyTrade(Long userId, TradeReqDto tradeReqDto) {
+        if (tradeReqDto.inputPrice() >= tradeReqDto.stockPrice()) {
+            stockTradeExecutor.executeTrade(userId, tradeReqDto);
+        } else {
+            tradeReservationHandler.saveTradeReservation(userId, tradeReqDto);
+        }
+    }
+
+    /**
+     * 주식을 파는 메서드.
+     * 사용자 입력 값 <= 주식 가격 : 주식 거래 성사
+     * 사용자 입력 값 > 주식 가격 : 주식 거래 예약
+     */
+    private void processSellTrade(Long userId, TradeReqDto tradeReqDto) {
+        if (tradeReqDto.inputPrice() <= tradeReqDto.stockPrice()) {
+            stockTradeExecutor.executeTrade(userId, tradeReqDto);
+        } else {
+            tradeReservationHandler.saveTradeReservation(userId, tradeReqDto);
+        }
+    }
+
 }
