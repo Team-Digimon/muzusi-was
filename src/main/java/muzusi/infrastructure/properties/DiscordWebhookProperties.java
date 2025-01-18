f7673b2f1473b40e0b6f9d47df5c4a6750d0ed8f
hky035 <rldud035@naver.com>
2025-01-19 00:44:10 +0900

    fix: 로컬 환경에서 애플리케이션 실행 시 오류로 인한 @Profile 어노테이션 삭제

--- src/main/java/muzusi/infrastructure/properties/DiscordWebhookProperties.java
+++ src/main/java/muzusi/infrastructure/properties/DiscordWebhookProperties.java
@@ -3,11 +3,9 @@ package muzusi.infrastructure.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("prod")
 @ConfigurationProperties(prefix = "webhook.discord")
 @Getter @Setter
 public class DiscordWebhookProperties {
