5a91fb8c74ebb426db7aea8db77779bf3e7b9554
DaeYoung0726 <pdy000726@naver.com>
2025-01-20 22:02:19 +0900

    test: 예약 매도 처리 로직 test 작성

--- src/test/java/muzusi/application/trade/service/TradeReservationHandlerTest.java
+++ src/test/java/muzusi/application/trade/service/TradeReservationHandlerTest.java
@@ -4,10 +4,12 @@ import muzusi.application.trade.dto.TradeReqDto;
 import muzusi.domain.account.entity.Account;
 import muzusi.domain.account.service.AccountService;
 import muzusi.domain.holding.entity.Holding;
+import muzusi.domain.holding.exception.HoldingErrorType;
 import muzusi.domain.holding.service.HoldingService;
 import muzusi.domain.trade.entity.TradeReservation;
 import muzusi.domain.trade.service.TradeReservationService;
 import muzusi.domain.trade.type.TradeType;
+import muzusi.global.exception.CustomException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
@@ -44,8 +47,8 @@ class TradeReservationHandlerTest {
 
     @BeforeEach
     void setUp() {
-        buyTradeDto = new TradeReqDto(3000L, 3000L, 5, "000610", TradeType.BUY);
-        sellTradeDto = new TradeReqDto(3000L, 3000L, 3, "000610", TradeType.SELL);
+        buyTradeDto = new TradeReqDto(3500L, 3000L, 5, "000610", TradeType.BUY);
+        sellTradeDto = new TradeReqDto(2500L, 3000L, 3, "000610", TradeType.SELL);
 
         account = Account.builder()
                 .balance(Account.INITIAL_BALANCE)
@@ -84,5 +87,23 @@ class TradeReservationHandlerTest {
 
         // then
         verify(tradeReservationService, times(1)).save(any(TradeReservation.class));
+    }
+
+    @Test
+    @DisplayName("매도 예약 실패 테스트 - 수량 부족")
+    void handleReservationSaleFalseTest() {
+        // given
+        TradeReqDto overCountDto = new TradeReqDto(2500L, 3000L, 15, "000610", TradeType.SELL);
+        given(holdingService.readByUserIdAndStockCode(1L, buyTradeDto.stockCode())).willReturn(Optional.of(holding));
+
+        // when
+        CustomException exception = assertThrows(CustomException.class, () ->
+                tradeReservationHandler.saveTradeReservation(1L, overCountDto)
+        );
+
+        // then
+        assertEquals(HoldingErrorType.INSUFFICIENT_STOCK.getStatus(), exception.getErrorType().getStatus());
+        assertEquals(HoldingErrorType.INSUFFICIENT_STOCK.getCode(), exception.getErrorType().getCode());
+        assertEquals(HoldingErrorType.INSUFFICIENT_STOCK.getMessage(), exception.getErrorType().getMessage());
     }
 }
