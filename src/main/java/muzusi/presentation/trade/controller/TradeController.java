9458c5c772fe1e80a78c176b467fec69a61f3755
DaeYoung0726 <pdy000726@naver.com>
2025-01-23 18:18:50 +0900

    feat: 예약 취소 Rest Api 연결

--- src/main/java/muzusi/presentation/trade/controller/TradeController.java
+++ src/main/java/muzusi/presentation/trade/controller/TradeController.java
@@ -9,6 +9,8 @@ import muzusi.global.security.auth.CustomUserDetails;
 import muzusi.presentation.trade.api.TradeApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,14 @@ public class TradeController implements TradeApi {
     public ResponseEntity<?> tradeStock(@AuthenticationPrincipal CustomUserDetails userDetails,
                                         @Valid @RequestBody TradeReqDto tradeReqDto) {
         stockTradeService.tradeStock(userDetails.getUserId(), tradeReqDto);
+
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+
+    @DeleteMapping("/{tradeReservationId}")
+    public ResponseEntity<?> cancelTradeReservation(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                    @PathVariable String tradeReservationId) {
+        stockTradeService.cancelTradeReservation(userDetails.getUserId(), tradeReservationId);
 
         return ResponseEntity.ok(SuccessResponse.ok());
     }
