d73e5e6fd5c8b5bc8b0f5c80c7e8dd760a53cd21
DaeYoung0726 <pdy000726@naver.com>
2025-01-25 15:40:51 +0900

    refact: News 반환 필드 id추가

--- src/main/java/muzusi/application/news/dto/NewsInfoDto.java
+++ src/main/java/muzusi/application/news/dto/NewsInfoDto.java
@@ -5,6 +5,7 @@ import muzusi.domain.news.entity.News;
 import java.time.LocalDateTime;
 
 public record NewsInfoDto(
+        Long id,
         String title,
         String link,
         String keyword,
@@ -12,6 +13,7 @@ public record NewsInfoDto(
 ) {
     public static NewsInfoDto from(News news) {
         return new NewsInfoDto(
+                news.getId(),
                 news.getTitle(),
                 news.getLink(),
                 news.getKeyword(),
