2825ea7c304cd391c7a3e01bcac10f80af089078
DaeYoung0726 <pdy000726@naver.com>
2025-01-27 01:47:26 +0900

    feat: Stock 차트 기록 컬렉션 생성

---
+++ src/main/java/muzusi/domain/stock/entity/StockWeekly.java
@@ -0,0 +1,41 @@
+package muzusi.domain.stock.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "stock_weekly")
+public class StockWeekly {
+    @Id
+    private String id;
+
+    private String stockCode;
+
+    private String date;
+
+    private Double open;
+
+    private Double high;
+
+    private Double low;
+
+    private Double close;
+
+    private Long volume;
+
+    @Builder
+    public StockWeekly(String stockCode, String date, Double open, Double high, Double low, Double close, Long volume) {
+        this.stockCode = stockCode;
+        this.date = date;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
+    }
+}
