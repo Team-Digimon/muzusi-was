3cf85dcb4c2c5664075cfd785d8c1b4ed36b79ad
DaeYoung0726 <pdy000726@naver.com>
2025-01-28 21:28:39 +0900

    feat: 계좌 id로 거래내역 조회 로직 구현

--- src/main/java/muzusi/domain/trade/repository/TradeRepository.java
+++ src/main/java/muzusi/domain/trade/repository/TradeRepository.java
@@ -3,5 +3,8 @@ package muzusi.domain.trade.repository;
 import muzusi.domain.trade.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TradeRepository extends JpaRepository<Trade, Long> {
+    List<Trade> findByAccount_Id(Long accountId);
 }
