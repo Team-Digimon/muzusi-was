fc58f2698c80fa1e6d61fe251535a7571abfb270
DaeYoung0726 <pdy000726@naver.com>
2025-01-29 01:54:06 +0900

    feat: 주식 이름 매핑을 위한 객체 생성

---
+++ src/main/java/muzusi/domain/stock/entity/StockItem.java
@@ -0,0 +1,11 @@
+package muzusi.domain.stock.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StockItem {
+    private String stockName;
+    private String stockCode;
+}
