e0a66c72410f55764bd9f34bb9e3a37a3482c9c9
DaeYoung0726 <pdy000726@naver.com>
2025-01-31 02:33:38 +0900

    feat: 매수내역 생성일자 추가

--- src/main/java/muzusi/domain/holding/entity/Holding.java
+++ src/main/java/muzusi/domain/holding/entity/Holding.java
@@ -2,6 +2,7 @@ package muzusi.domain.holding.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,9 +16,14 @@ import lombok.NoArgsConstructor;
 import muzusi.domain.account.entity.Account;
 import muzusi.domain.user.entity.User;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity(name = "holding")
 @DynamicUpdate
 public class Holding {
@@ -36,6 +42,10 @@ public class Holding {
 
     @Column(name = "average_price", nullable = false)
     private Long averagePrice;
+
+    @Column(name = "holding_at")
+    @CreatedDate
+    private LocalDateTime holdingAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
