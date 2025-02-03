0967073ba4a3ce4e07648350fa36ba6aae5d2ebe
hky035 <rldud035@naver.com>
2025-02-03 14:51:25 +0900

    feat: 스케줄링 전용 쓰레드풀 설정

--- src/main/java/muzusi/infrastructure/config/SchedulingConfig.java
+++ src/main/java/muzusi/infrastructure/config/SchedulingConfig.java
@@ -1,9 +1,22 @@
 package muzusi.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
 public class SchedulingConfig {
+    private final static int THREAD_POOL_SIZE = 3;
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(THREAD_POOL_SIZE);
+        taskScheduler.setThreadNamePrefix("Scheduling-thread-");
+        taskScheduler.initialize();
+        return taskScheduler;
+    }
 }
