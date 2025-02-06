364bedcb6b3593d5676f480e25e4c6ccd5bb5418
DaeYoung0726 <pdy000726@naver.com>
2025-02-06 19:21:24 +0900

    feat: 예약 생성 및 삭제 시 redis에 종목 코드 생성 및 삭제

--- src/main/java/muzusi/application/trade/service/TradeReservationHandler.java
+++ src/main/java/muzusi/application/trade/service/TradeReservationHandler.java
@@ -12,6 +12,8 @@ import muzusi.domain.trade.entity.TradeReservation;
 import muzusi.domain.trade.exception.TradeErrorType;
 import muzusi.domain.trade.service.TradeReservationService;
 import muzusi.global.exception.CustomException;
+import muzusi.infrastructure.redis.RedisService;
+import muzusi.infrastructure.redis.constant.TradeConstant;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,6 +22,7 @@ public class TradeReservationHandler {
     private final TradeReservationService tradeReservationService;
     private final AccountService accountService;
     private final HoldingService holdingService;
+    private final RedisService redisService;
 
     /**
      * 성사가 안된 주식 거래를 예약 처리하는 메서드
@@ -34,16 +37,7 @@ public class TradeReservationHandler {
             default -> throw new IllegalArgumentException("잘못된 거래 유형입니다.");
         }
 
-        TradeReservation reservation = TradeReservation.builder()
-                .userId(userId)
-                .inputPrice(tradeReqDto.inputPrice())
-                .stockCount(tradeReqDto.stockCount())
-                .stockName(tradeReqDto.stockName())
-                .stockCode(tradeReqDto.stockCode())
-                .tradeType(tradeReqDto.tradeType())
-                .build();
-
-        tradeReservationService.save(reservation);
+        finalizeSaveReservation(userId, tradeReqDto);
     }
 
     /**
@@ -75,6 +69,29 @@ public class TradeReservationHandler {
     }
 
     /**
+     * 예약 처리 완료 후 처리
+     * 1. 예약 내역 db 저장
+     * 2. 예약된 종목 코드 redis 저장 (종목 코드에 대한 주식 현재가 불러오기 위함)
+     *
+     * @param userId : 사용자 pk값
+     * @param tradeReqDto : trade 정보 dto
+     */
+    private void finalizeSaveReservation(Long userId, TradeReqDto tradeReqDto) {
+        TradeReservation reservation = TradeReservation.builder()
+                .userId(userId)
+                .inputPrice(tradeReqDto.inputPrice())
+                .stockCount(tradeReqDto.stockCount())
+                .stockName(tradeReqDto.stockName())
+                .stockCode(tradeReqDto.stockCode())
+                .tradeType(tradeReqDto.tradeType())
+                .build();
+
+        tradeReservationService.save(reservation);
+
+        redisService.addToSet(TradeConstant.RESERVATION_PREFIX.getValue(), tradeReqDto.stockCode());
+    }
+
+    /**
      * 예약된 주식 거래를 취소하는 메서드
      *
      * @param userId : 사용자 pk값
@@ -94,7 +111,7 @@ public class TradeReservationHandler {
             default -> throw new IllegalArgumentException("잘못된 거래 유형입니다.");
         }
 
-        tradeReservationService.deleteById(tradeReservationId);
+        finalizeCancelReservation(tradeReservationId, reservation.getStockCode());
     }
 
     /**
@@ -118,5 +135,21 @@ public class TradeReservationHandler {
                 .orElseThrow(() -> new CustomException(HoldingErrorType.NOT_FOUND));
 
         holding.decreaseReservedStock(reservation.getStockCount());
+    }
+
+    /**
+     * 예약 취소 완료 후 처리
+     * 1. 예약 내역 삭제
+     * 2. 종목 코드에 예약 내역 없으면, redis 값 삭제
+     *
+     * @param tradeReservationId : 예약 내역 id
+     * @param stockCode : 주식 종목 코드
+     */
+    private void finalizeCancelReservation(String tradeReservationId, String stockCode) {
+        tradeReservationService.deleteById(tradeReservationId);
+
+        if (!tradeReservationService.existsByStockCode(stockCode)) {
+            redisService.removeFromSet(TradeConstant.RESERVATION_PREFIX.getValue(), stockCode);
+        }
     }
 }
