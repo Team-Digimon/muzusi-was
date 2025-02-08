1fbf9ebfb32c0a18b0e8dcd555b41084b96930f7
hky035 <rldud035@naver.com>
2025-02-09 02:18:48 +0900

    feat: 주식 분봉 데이터 저장 스케줄링 적용

--- src/main/java/muzusi/application/kis/scheduler/KisScheduler.java
+++ src/main/java/muzusi/application/kis/scheduler/KisScheduler.java
@@ -3,14 +3,21 @@ package muzusi.application.kis.scheduler;
 import lombok.RequiredArgsConstructor;
 import muzusi.application.kis.service.KisOAuthService;
 import muzusi.application.kis.service.KisRankingService;
+import muzusi.application.kis.service.KisStockService;
+import muzusi.domain.stock.service.StockMinutesService;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
 public class KisScheduler {
     private final KisOAuthService kisOAuthService;
     private final KisRankingService kisRankingService;
+    private final KisStockService kisStockService;
+    private final StockMinutesService stockMinutesService;
 
     @Scheduled(cron = "0 0 7 * * ?")
     public void runIssueAccessTokenJob() {
@@ -32,5 +39,20 @@ public class KisScheduler {
     public void runRankingJobAt3PM() {
         kisRankingService.saveVolumeRank();
         kisRankingService.saveFluctuationRank();
+    }
+
+    @Schedules({
+            @Scheduled(cron = "0 10,20,30,40,50 9 * * 1-5"),
+            @Scheduled(cron = "0 0/10 10-14 * * 1-5"),
+            @Scheduled(cron = "0 0,10,20,30 15 * * 1-5")
+    })
+    public void runSaveStockMinutesChart() throws InterruptedException {
+        kisStockService.saveStockMinutesChart();
+    }
+
+    @Scheduled(cron = "0 0 16 * * 1-5")
+    public void runSaveDailyStockMinutesChartJob() {
+        kisStockService.saveDailyStockMinutesChart();
+        stockMinutesService.deleteByDateBefore(LocalDate.now().minusDays(6));
     }
 }
