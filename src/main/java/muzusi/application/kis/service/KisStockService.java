a5ff3567792fbe478595860463e61b70238b307d
hky035 <rldud035@naver.com>
2025-02-09 02:11:43 +0900

    feat: 일별 주식 분봉 데이터 저장 메서드 추가

--- src/main/java/muzusi/application/kis/service/KisStockService.java
+++ src/main/java/muzusi/application/kis/service/KisStockService.java
@@ -2,13 +2,19 @@ package muzusi.application.kis.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import muzusi.application.stock.dto.StockMinutesChartInfoDto;
+import muzusi.domain.stock.entity.StockMinutes;
+import muzusi.domain.stock.service.StockMinutesService;
 import muzusi.infrastructure.data.StockCodeProvider;
 import muzusi.infrastructure.kis.KisStockClient;
 import muzusi.infrastructure.redis.RedisService;
 import muzusi.infrastructure.redis.constant.KisConstant;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -17,6 +23,7 @@ public class KisStockService {
     private final StockCodeProvider stockCodeProvider;
     private final RedisService redisService;
     private final KisStockClient kisStockClient;
+    private final StockMinutesService stockMinutesService;
 
     public void saveStockMinutesChart() throws InterruptedException {
         int count = 0;
@@ -26,6 +33,20 @@ public class KisStockService {
                 Thread.sleep(1000L);
             }
             redisService.setList(KisConstant.MINUTES_CHART_PREFIX.getValue() + ":" + code, kisStockClient.getStockMinutesChartInfo(code, now));
+        }
+    }
+
+    public void saveDailyStockMinutesChart() {
+        for (String code : stockCodeProvider.getAllStockCodes()) {
+            List<StockMinutesChartInfoDto> minutesChars = redisService.getList(KisConstant.MINUTES_CHART_PREFIX.getValue() + ":" + code).stream()
+                            .map(stockMinutesChart -> (StockMinutesChartInfoDto) stockMinutesChart)
+                            .toList();
+
+            stockMinutesService.save(StockMinutes.builder()
+                    .stockCode(code)
+                    .date(LocalDate.now(ZoneId.of("Asia/Seoul")))
+                    .minutesChart(minutesChars)
+                    .build());
         }
     }
 }
