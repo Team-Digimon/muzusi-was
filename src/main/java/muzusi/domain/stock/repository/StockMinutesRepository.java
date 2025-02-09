7d1ae7da6b57cd05d843e18d328ec1ca6cfcc313
hky035 <rldud035@naver.com>
2025-02-09 16:54:15 +0900

    fix: StockMinutesRepository 상위 클래스 변경

--- src/main/java/muzusi/domain/stock/repository/StockMinutesRepository.java
+++ src/main/java/muzusi/domain/stock/repository/StockMinutesRepository.java
@@ -1,12 +1,12 @@
 package muzusi.domain.stock.repository;
 
 import muzusi.domain.stock.entity.StockMinutes;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface StockMinutesRepository extends CrudRepository<StockMinutes, Integer> {
+public interface StockMinutesRepository extends MongoRepository<StockMinutes, String> {
     List<StockMinutes> findByStockCodeOrderByDateAsc(String stockCode);
 
     void deleteByDateBefore(LocalDate date);
