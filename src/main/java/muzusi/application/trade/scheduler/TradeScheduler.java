f27e119f956f0a60ca0635aa70d72c94f0ad5a09
DaeYoung0726 <pdy000726@naver.com>
2025-02-13 02:57:40 +0900

    feat: 예약 확인 스케쥴링 설정

--- src/main/java/muzusi/application/trade/scheduler/TradeScheduler.java
+++ src/main/java/muzusi/application/trade/scheduler/TradeScheduler.java
@@ -2,16 +2,28 @@ package muzusi.application.trade.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import muzusi.application.trade.service.TradeReservationCleaner;
+import muzusi.application.trade.service.TradeReservationTrigger;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class TradeScheduler {
     private final TradeReservationCleaner tradeReservationCleaner;
+    private final TradeReservationTrigger tradeReservationTrigger;
 
-    @Scheduled(cron = "0 30 15 * * MON-FRI")
+    @Scheduled(cron = "0 30 15 * * 1-5")
     public void runReservationDeleteJob() {
         tradeReservationCleaner.clearReservedOrdersAtMarketClose();
+    }
+
+    @Schedules({
+            @Scheduled(cron = "0 15,25,35,45,55 9 * * 1-5"),
+            @Scheduled(cron = "0 5,15,25,35,45,55 10-14 * * 1-5"),
+            @Scheduled(cron = "0 5,15,25 15 * * 1-5")
+    })
+    public void runReservationTriggerJob() {
+        tradeReservationTrigger.triggerTradeReservations();
     }
 }
