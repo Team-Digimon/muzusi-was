0bde720975dae3ccc2cc05874d71055b12627a7f
DaeYoung0726 <pdy000726@naver.com>
2025-02-14 02:51:02 +0900

    refact: 불필요 JOIN문 삭제

--- src/main/java/muzusi/domain/holding/repository/HoldingRepository.java
+++ src/main/java/muzusi/domain/holding/repository/HoldingRepository.java
@@ -9,17 +9,15 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface HoldingRepository extends JpaRepository<Holding, Long> {
-    @Query(value = "SELECT h.* FROM holding h " +
-            "JOIN account a ON h.account_id = a.id " +
-            "WHERE a.id = (SELECT id FROM account WHERE user_id = :userId ORDER BY created_at DESC LIMIT 1) " +
-            "AND h.stock_code = :stockCode ", nativeQuery = true)
+    @Query(value = "SELECT * FROM holding " +
+            "WHERE account_id = (SELECT id FROM account WHERE user_id = :userId ORDER BY created_at DESC LIMIT 1) " +
+            "AND stock_code = :stockCode", nativeQuery = true)
     Optional<Holding> findLatestAccountHolding(@Param("userId") Long userId, @Param("stockCode") String stockCode);
 
     @Query(value = "SELECT EXISTS ( " +
-            "SELECT 1 FROM holding h " +
-            "JOIN account a ON h.account_id = a.id " +
-            "WHERE a.id = (SELECT id FROM account WHERE user_id = :userId ORDER BY created_at DESC LIMIT 1) " +
-            "AND h.stock_code = :stockCode) ", nativeQuery = true)
+            "SELECT 1 FROM holding " +
+            "WHERE account_id = (SELECT id FROM account WHERE user_id = :userId ORDER BY created_at DESC LIMIT 1) " +
+            "AND stock_code = :stockCode )", nativeQuery = true)
     Integer existsByLatestAccountHolding(@Param("userId") Long userId, @Param("stockCode") String stockCode);
 
     @Modifying
