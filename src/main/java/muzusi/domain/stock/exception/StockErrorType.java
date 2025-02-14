488076f1ee17ae058b33b3a114a1a2d1a893901d
hky035 <rldud035@naver.com>
2025-02-15 00:24:24 +0900

    feat: 주식 분봉 조회 시 발생가능 커스텀 에러 타입 추가

--- src/main/java/muzusi/domain/stock/exception/StockErrorType.java
+++ src/main/java/muzusi/domain/stock/exception/StockErrorType.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum StockErrorType implements BaseErrorType {
     NOT_FOUND(HttpStatus.NOT_FOUND, "2001", "주식 종목이 존재하지 않습니다."),
-    NOT_PREPARED(HttpStatus.NOT_FOUND, "2002", "아직 준비 중인 주식 종목입니다.")
+    NOT_PREPARED(HttpStatus.NOT_FOUND, "2002", "아직 준비 중인 주식 종목입니다."),
+    NOT_AVAILABLE_MINUTES_CHART(HttpStatus.BAD_REQUEST, "2003", "당일 주식 조회 가능 기간이 아닙니다."),
+    UNSUPPORTED_MINUTES_PERIOD(HttpStatus.BAD_REQUEST, "2004", "주식 분봉 데이터 조회 시간이 아닙니다.")
     ;
 
     private final HttpStatus status;
