5d723cac3b870dc51b9bcd2947d9eebaa62e7505
hky035 <rldud035@naver.com>
2025-02-15 16:34:55 +0900

    feat: 차트 시간 데이터 직렬화 및 JSON 포맷팅 어노테이션 추가

--- src/main/java/muzusi/application/stock/dto/StockChartInfoDto.java
+++ src/main/java/muzusi/application/stock/dto/StockChartInfoDto.java
@@ -1,5 +1,10 @@
 package muzusi.application.stock.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import muzusi.domain.stock.entity.StockDaily;
 import muzusi.domain.stock.entity.StockMonthly;
@@ -11,6 +16,9 @@ import java.time.LocalDateTime;
 @Builder
 public record StockChartInfoDto(
         String stockCode,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
         LocalDateTime date,
         Long open,
         Long high,
