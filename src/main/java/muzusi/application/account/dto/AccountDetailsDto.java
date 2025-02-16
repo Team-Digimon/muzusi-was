fd9f990ae2db0d7df53c98873e16ae3730c3ab99
DaeYoung0726 <pdy000726@naver.com>
2025-02-16 19:33:03 +0900

    feat: 계좌 생성시간 반환 추가

--- src/main/java/muzusi/application/account/dto/AccountDetailsDto.java
+++ src/main/java/muzusi/application/account/dto/AccountDetailsDto.java
@@ -2,10 +2,13 @@ package muzusi.application.account.dto;
 
 import muzusi.domain.account.entity.Account;
 
+import java.time.LocalDateTime;
+
 public record AccountDetailsDto(
         Long id,
         long balance,
         long reservedPrice,
+        LocalDateTime createdAt,
         double totalRateOfReturn,
         long totalEvaluatedAmount,
         long totalProfitAmount
@@ -15,6 +18,7 @@ public record AccountDetailsDto(
                 account.getId(),
                 account.getBalance(),
                 account.getReservedPrice(),
+                account.getCreatedAt(),
                 summary.totalRateOfReturn(),
                 summary.totalEvaluatedAmount(),
                 summary.totalProfitAmount()
