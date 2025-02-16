e98813ceb8569468b2993659525ed2806a0b69dd
DaeYoung0726 <pdy000726@naver.com>
2025-02-16 19:24:24 +0900

    docs: 보유 주식 조회 swagger 작성

--- src/main/java/muzusi/presentation/holding/controller/HoldingController.java
+++ src/main/java/muzusi/presentation/holding/controller/HoldingController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import muzusi.application.holding.service.UserHoldingService;
 import muzusi.global.response.success.SuccessResponse;
 import muzusi.global.security.auth.CustomUserDetails;
+import muzusi.presentation.holding.api.HoldingApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/accounts")
 @RequiredArgsConstructor
-public class HoldingController {
+public class HoldingController implements HoldingApi {
     private final UserHoldingService userHoldingService;
 
+    @Override
     @GetMapping("/holdings")
     public ResponseEntity<?> getHoldings(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseEntity.ok(
