b4bdd91a65efe1babf2c87762e8ef8b38526e2f3
hky035 <rldud035@naver.com>
2025-02-19 13:37:43 +0900

    feat: 한국투자증권 웹소켓 연결 스케줄러 추가

---
+++ src/main/java/muzusi/application/websocket/scheduler/WebSocketConnectionScheduler.java
@@ -0,0 +1,52 @@
+package muzusi.application.websocket.scheduler;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import muzusi.application.kis.service.KisWebSocketHandler;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.client.WebSocketConnectionManager;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+@Component
+@DependsOn("kisWebSocketConfig")
+@RequiredArgsConstructor
+public class WebSocketConnectionScheduler {
+    private final WebSocketConnectionManager connection;
+    private final KisWebSocketHandler kisRealTimeTradeHandler;
+
+    @PostConstruct
+    public void init() {
+        LocalDateTime now = LocalDateTime.now();
+        DayOfWeek dayOfWeek = now.getDayOfWeek();
+        boolean isWeek = dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
+        boolean isEnabled = ((now.getHour() > 8) || (now.getHour() == 8 && now.getMinute() >= 55))
+                                && ((now.getHour() < 15) || (now.getHour() == 15 && now.getMinute() < 30));
+
+        if (!isWeek && isEnabled && !connection.isConnected())
+            connection.start();
+    }
+
+    @Scheduled(cron = "0 59 8 * * 1-5")
+    public void runConnectWebSocketSessionJob() {
+        connection.start();
+    }
+
+    @Scheduled(cron = "0 30 15 * * 1-5")
+    public void runDisconnectWebSocketToKis() {
+        if (connection.isConnected())
+            connection.stop();
+    }
+
+    @Schedules({
+            @Scheduled(cron = "0 * 9-14 * * 1-5"),
+            @Scheduled(cron = "0 0-30 15 * * 1-5")
+    })
+    public void runKeepConnectionJob() {
+        kisRealTimeTradeHandler.keepConnection();
+    }
+}
