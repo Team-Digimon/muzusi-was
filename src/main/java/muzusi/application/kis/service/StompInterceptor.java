32004051ddc785644e0cdb1141c79b515c9b9beb
hky035 <rldud035@naver.com>
2025-02-19 13:36:51 +0900

    feat: 웹소켓 메시지 인터셉터 추가

---
+++ src/main/java/muzusi/application/kis/service/StompInterceptor.java
@@ -0,0 +1,32 @@
+package muzusi.application.kis.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StompInterceptor implements ChannelInterceptor {
+    private final KisRealTimeTradeHandler kisRealTimeTradeHandler;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+            String stockCode = accessor.getDestination().replace("/sub/","");
+            kisRealTimeTradeHandler.connect(stockCode);
+        }
+
+        if (StompCommand.UNSUBSCRIBE.equals(accessor.getCommand())) {
+            String stockCode = accessor.getDestination().replace("/unsub/","");
+            kisRealTimeTradeHandler.disconnect(stockCode);
+        }
+
+        return ChannelInterceptor.super.preSend(message, channel);
+    }
+}
