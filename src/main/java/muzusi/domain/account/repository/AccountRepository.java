42c9a2fc1e9f5ac4b2b0d868bb354c20fe795504
DaeYoung0726 <pdy000726@naver.com>
2025-02-26 17:07:01 +0900

    refact: account jpql 문구 QueryDsl로 변경

--- src/main/java/muzusi/domain/account/repository/AccountRepository.java
+++ src/main/java/muzusi/domain/account/repository/AccountRepository.java
@@ -2,32 +2,9 @@ package muzusi.domain.account.repository;
 
 import muzusi.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long>, CustomAccountRepository {
     List<Account> findByUser_Id(Long userId);
-
-    @Query(value = "SELECT * FROM account a " +
-            "WHERE a.user_id = :userId " +
-            "ORDER BY a.created_at DESC LIMIT 1", nativeQuery = true)
-    Optional<Account> findLatestAccount(@Param("userId") Long userId);
-
-    @Query(value = "SELECT a.created_at FROM account a " +
-            "WHERE a.user_id = :userId " +
-            "ORDER BY a.created_at DESC LIMIT 1", nativeQuery = true)
-    LocalDateTime findLatestCreatedAt(@Param("userId") Long userId);
-
-    @Query(value = "SELECT * FROM account a " +
-            "WHERE a.id = ( " +
-            "    SELECT a2.id FROM account a2 " +
-            "    WHERE a2.user_id = a.user_id " +
-            "    ORDER BY a2.created_at DESC " +
-            "    LIMIT 1 " +
-            ")", nativeQuery = true)
-    List<Account> findLatestAccountsForAllUsers();
 }
---
+++ src/main/java/muzusi/domain/account/repository/CustomAccountRepositoryImpl.java
@@ -0,0 +1,56 @@
+package muzusi.domain.account.repository;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import muzusi.domain.account.entity.Account;
+import muzusi.domain.account.entity.QAccount;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class CustomAccountRepositoryImpl implements CustomAccountRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private final QAccount account = QAccount.account;
+
+    @Override
+    public Optional<Account> findLatestAccount(Long userId) {
+        return Optional.ofNullable(
+                jpaQueryFactory
+                        .selectFrom(account)
+                        .where(account.user.id.eq(userId))
+                        .orderBy(account.createdAt.desc())
+                        .limit(1)
+                        .fetchOne()
+        );
+    }
+
+    @Override
+    public LocalDateTime findLatestCreatedAt(Long userId) {
+        return jpaQueryFactory
+                .select(account.createdAt)
+                .from(account)
+                .where(account.user.id.eq(userId))
+                .orderBy(account.createdAt.desc())
+                .limit(1)
+                .fetchOne();
+    }
+
+    @Override
+    public List<Account> findLatestAccountsForAllUsers() {
+        QAccount subAccount = new QAccount("subAccount");
+
+        return jpaQueryFactory
+                .selectFrom(account)
+                .where(account.createdAt.eq(
+                        JPAExpressions
+                                .select(subAccount.createdAt.max())
+                                .from(subAccount)
+                                .where(subAccount.user.id.eq(account.user.id))
+                ))
+                .fetch();
+    }
+}
