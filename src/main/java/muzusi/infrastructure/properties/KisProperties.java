5b26f1d6f304e5b3818e193721d02cac686f27d6
hky035 <rldud035@naver.com>
2025-03-05 02:47:33 +0900

    feat: 한국투자증권 인증키 환경변수 바인딩 방식 변경
     * String -> List<String>

--- src/main/java/muzusi/infrastructure/properties/KisProperties.java
+++ src/main/java/muzusi/infrastructure/properties/KisProperties.java
@@ -6,14 +6,16 @@ import muzusi.infrastructure.kis.constant.KisUrlConstant;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @ConfigurationProperties(prefix = "kis")
 @Getter @Setter
 public class KisProperties {
     private String domain;
     private String webSocketDomain;
-    private String appKey;
-    private String appSecret;
+    private List<String> appKey;
+    private List<String> appSecret;
 
     public String getUrl(KisUrlConstant kisUrlConstant) {
         return domain + kisUrlConstant.getUrl();
