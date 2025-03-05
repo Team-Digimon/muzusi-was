806abc9ec3393d4e84478da4414d39bc1494907e
hky035 <rldud035@naver.com>
2025-03-05 23:03:44 +0900

    feat: 한국투자증권 웹소켓 연결 스케줄링

--- src/main/java/muzusi/application/websocket/scheduler/WebSocketConnectionScheduler.java
+++ src/main/java/muzusi/application/websocket/scheduler/WebSocketConnectionScheduler.java
@@ -2,22 +2,29 @@ package muzusi.application.websocket.scheduler;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import muzusi.infrastructure.kis.KisRealTimeTradeHandler;
+import muzusi.infrastructure.kis.KisAuthService;
+import muzusi.infrastructure.kis.websocket.KisWebSocketConnector;
+import muzusi.infrastructure.kis.websocket.KisWebSocketSessionManager;
+import muzusi.infrastructure.kis.websocket.Session;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 @DependsOn("kisWebSocketConfig")
 @RequiredArgsConstructor
 public class WebSocketConnectionScheduler {
-    private final WebSocketConnectionManager connection;
-    private final KisRealTimeTradeHandler kisRealTimeTradeHandler;
+    private final List<WebSocketConnectionManager> connections;
+    private final KisWebSocketSessionManager kisWebSocketSessionManager;
+    private final KisWebSocketConnector kisWebSocketConnector;
+    private final KisAuthService kisAuthService;
 
     @PostConstruct
     public void init() {
@@ -27,22 +34,33 @@ public class WebSocketConnectionScheduler {
         boolean isEnabled = ((now.getHour() > 8) || (now.getHour() == 8 && now.getMinute() >= 55))
                             && ((now.getHour() < 15) || (now.getHour() == 15 && now.getMinute() < 30));
 
-        if (!isWeek && isEnabled && !connection.isConnected())
+        for (WebSocketConnectionManager connection : connections) {
+            if (!isWeek && isEnabled && !connection.isConnected())
+                connection.start();
+        }
+    }
+
+    @Scheduled(cron = "30 58 8 * * 1-5")
+    public void runConnectWebSocketSessionJob() {
+        for (WebSocketConnectionManager connection : connections)
             connection.start();
     }
 
     @Scheduled(cron = "0 59 8 * * 1-5")
-    public void runConnectWebSocketSessionJob() {
-        connection.start();
+    public void runSaveWebSocketKeyJob() {
+        kisWebSocketSessionManager.addWebSocketKey(kisAuthService.getWebSocketKey());
     }
 
     @Scheduled(cron = "0 30 15 * * 1-5")
     public void runDisconnectWebSocketToKis() {
-        kisRealTimeTradeHandler.getConnectingStockCodes()
-                .forEach(kisRealTimeTradeHandler::disconnect);
+        for (Session session : kisWebSocketSessionManager.getSessions()) {
+            session.clearSubscription();
+        }
 
-        if (connection.isConnected())
-            connection.stop();
+        for (WebSocketConnectionManager connection : connections) {
+            if (connection.isConnected())
+                connection.stop();
+        }
     }
 
     @Schedules({
@@ -50,6 +68,8 @@ public class WebSocketConnectionScheduler {
             @Scheduled(cron = "0 0-29 15 * * 1-5")
     })
     public void runKeepConnectionJob() {
-        kisRealTimeTradeHandler.keepConnection();
+        for (WebSocketSession session : kisWebSocketSessionManager.getWebSocketSessions()) {
+            kisWebSocketConnector.keepConnection(session);
+        }
     }
 }
