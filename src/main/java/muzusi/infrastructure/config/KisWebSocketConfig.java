a5b3ef307538a15c019c65ee0be25272b11b4f44
hky035 <rldud035@naver.com>
2025-03-05 22:45:48 +0900

    feat: 2개 세션 사용을 위한 WebSocketConnectionManager Bean 등록

--- src/main/java/muzusi/infrastructure/config/KisWebSocketConfig.java
+++ src/main/java/muzusi/infrastructure/config/KisWebSocketConfig.java
@@ -1,10 +1,11 @@
 package muzusi.infrastructure.config;
 
 import lombok.RequiredArgsConstructor;
-import muzusi.infrastructure.kis.KisRealTimeTradeHandler;
+import muzusi.infrastructure.kis.websocket.KisWebSocketRouter;
 import muzusi.infrastructure.properties.KisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
@@ -16,14 +17,24 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class KisWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final KisProperties kisProperties;
-    private final KisRealTimeTradeHandler kisRealTimeTradeHandler;
+    private final KisWebSocketRouter kisWebSocketRouter;
+    private final WebSocketClient client = new StandardWebSocketClient();
 
     @Bean
-    public WebSocketConnectionManager webSocketConnectionManager() {
-        WebSocketClient client = new StandardWebSocketClient();
+    @Primary
+    public WebSocketConnectionManager webSocketConnectionManager1() {
         return new WebSocketConnectionManager(
                 client,
-                kisRealTimeTradeHandler,
+                kisWebSocketRouter,
+                kisProperties.getWebSocketDomain()
+        );
+    }
+
+    @Bean
+    public WebSocketConnectionManager webSocketConnectionManager2() {
+        return new WebSocketConnectionManager(
+                client,
+                kisWebSocketRouter,
                 kisProperties.getWebSocketDomain()
         );
     }
