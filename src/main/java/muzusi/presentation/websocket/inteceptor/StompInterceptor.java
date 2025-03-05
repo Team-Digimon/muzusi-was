873d1260ddcdb56245e934a40dfb146bff836c56
hky035 <rldud035@naver.com>
2025-03-05 23:02:11 +0900

    feat: STOMP 메시지 요청값 변경에 따른 코드 변경
     * stockCode -> trType, stockCode

--- src/main/java/muzusi/presentation/websocket/inteceptor/StompInterceptor.java
+++ src/main/java/muzusi/presentation/websocket/inteceptor/StompInterceptor.java
@@ -2,7 +2,8 @@ package muzusi.presentation.websocket.inteceptor;
 
 import lombok.RequiredArgsConstructor;
 import muzusi.application.stock.service.StockSearchService;
-import muzusi.infrastructure.kis.KisRealTimeTradeHandler;
+import muzusi.infrastructure.kis.websocket.KisWebSocketConnector;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -13,10 +14,11 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class StompInterceptor implements ChannelInterceptor {
-    private final KisRealTimeTradeHandler kisRealTimeTradeHandler;
+    private final KisWebSocketConnector kisWebSocketConnector;
     private final StockSearchService stockSearchService;
 
     private final static String STOCK_CODE_HEADER_NAME = "stockCode";
+    private final static String TR_ID_HEADER_NAME = "trId";
 
     /**
      * 특정 종목 구독 및 해제 시 한국투자증권 웹소켓 연결 관리를 위한 메서드
@@ -31,20 +33,28 @@ public class StompInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        String stockCode = extractStockCode(accessor);
+        Pair<String, String> headers = extractCustomHeader(accessor);
 
         if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
-            stockSearchService.increaseStockSearchCount(stockCode);
-            kisRealTimeTradeHandler.connect(stockCode);
+            stockSearchService.increaseStockSearchCount(headers.getRight());
+            kisWebSocketConnector.subscribe(headers.getLeft(), headers.getRight());
         }
 
         if (StompCommand.UNSUBSCRIBE.equals(accessor.getCommand()))
-            kisRealTimeTradeHandler.disconnect(stockCode);
+            kisWebSocketConnector.unsubscribe(headers.getLeft(), headers.getRight());
 
         return message;
     }
 
-    private String extractStockCode(StompHeaderAccessor accessor) {
-        return accessor.getFirstNativeHeader(STOCK_CODE_HEADER_NAME);
+    /**
+     *
+     * @param accessor : Stomp 메시지 헤더 접근자
+     * @return : 커스텀 헤더명 (trId, stockCode)
+     */
+    private Pair<String, String> extractCustomHeader(StompHeaderAccessor accessor) {
+        return Pair.of(
+                accessor.getFirstNativeHeader(TR_ID_HEADER_NAME),
+                accessor.getFirstNativeHeader(STOCK_CODE_HEADER_NAME)
+        );
     }
 }
