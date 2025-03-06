0adc67237ed0d113111497c007ff9c83f323547d
DaeYoung0726 <pdy000726@naver.com>
2025-03-06 18:42:07 +0900

    refact: 거래 예약 RDB로 변경

--- src/main/java/muzusi/domain/trade/entity/TradeReservation.java
+++ src/main/java/muzusi/domain/trade/entity/TradeReservation.java
@@ -1,35 +1,51 @@
 package muzusi.domain.trade.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import muzusi.domain.trade.type.TradeType;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Document(collection = "trade_reservation")
+@EntityListeners(AuditingEntityListener.class)
+@Entity(name = "trade_reservation")
 public class TradeReservation {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "input_price")
     private Long inputPrice;
 
+    @Column(name = "input_count")
     private Integer stockCount;
 
+    @Column(name = "stock_name")
     private String stockName;
 
+    @Column(name = "stock_code")
     private String stockCode;
 
+    @Enumerated(EnumType.STRING)
     private TradeType tradeType;
 
+    @Column(name = "created_at")
     @CreatedDate
     private LocalDateTime createdAt;
 
