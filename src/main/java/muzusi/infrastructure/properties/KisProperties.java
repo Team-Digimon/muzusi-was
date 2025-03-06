a25aa6bf54137c4eece530b2456ce6d0aef6fee1
hky035 <rldud035@naver.com>
2025-03-06 11:36:05 +0900

    refact: 한국투자증권 인증키 리스트 반환 메서드 위치 변경
     * KisOAuthClient -> KisProperties

--- src/main/java/muzusi/infrastructure/properties/KisProperties.java
+++ src/main/java/muzusi/infrastructure/properties/KisProperties.java
@@ -3,9 +3,11 @@ package muzusi.infrastructure.properties;
 import lombok.Getter;
 import lombok.Setter;
 import muzusi.infrastructure.kis.constant.KisUrlConstant;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -19,5 +21,15 @@ public class KisProperties {
 
     public String getUrl(KisUrlConstant kisUrlConstant) {
         return domain + kisUrlConstant.getUrl();
+    }
+
+    public List<Pair<String, String>> getAuthKeys() {
+        List<Pair<String, String>> authKeys = new ArrayList<>();
+
+        for (int idx = 0; idx < Math.min(appKey.size(), appSecret.size()); idx++) {
+            authKeys.add(Pair.of(appKey.get(idx), appSecret.get(idx)));
+        }
+
+        return authKeys;
     }
 }
