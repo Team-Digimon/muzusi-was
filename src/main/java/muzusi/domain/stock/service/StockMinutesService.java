c0b48593bc1f47a9042c806e0419abdec0157370
hky035 <rldud035@naver.com>
2025-03-16 12:36:32 +0900

    feat: stockMinutesService 내 캐싱 메서드 추가

--- src/main/java/muzusi/domain/stock/service/StockMinutesService.java
+++ src/main/java/muzusi/domain/stock/service/StockMinutesService.java
@@ -1,17 +1,21 @@
 package muzusi.domain.stock.service;
 
 import lombok.RequiredArgsConstructor;
+import muzusi.application.stock.dto.StockChartInfoDto;
 import muzusi.domain.stock.entity.StockMinutes;
+import muzusi.domain.stock.repository.StockMinutesCacheRepository;
 import muzusi.domain.stock.repository.StockMinutesRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StockMinutesService {
     private final StockMinutesRepository stockMinutesRepository;
+    private final StockMinutesCacheRepository stockMinutesCacheRepository;
 
     public void save(StockMinutes stockMinutes) {
         stockMinutesRepository.save(stockMinutes);
@@ -27,5 +31,17 @@ public class StockMinutesService {
 
     public void deleteByDateBefore(LocalDate date) {
         stockMinutesRepository.deleteByDateBefore(date);
+    }
+
+    public void saveAllInCache(Collection<StockChartInfoDto> stockChartInfoList) {
+        stockMinutesCacheRepository.saveAll(stockChartInfoList);
+    }
+
+    public List<StockChartInfoDto> readAllInCache(String stockCode) {
+        return stockMinutesCacheRepository.findAll(stockCode);
+    }
+
+    public void deleteInCache(String stockCode) {
+        stockMinutesCacheRepository.delete(stockCode);
     }
 }
