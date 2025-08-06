804810d51da42b1a63f4b86221cdfbc2a76ae25a
hky035 <rldud035@naver.com>
2025-08-07 03:25:14 +0900

    refact: KisOAuthService 클래스명 변경
     * 클래스 역할과 책임을 명확히 반영하여 명확성을 향상

--- src/main/java/muzusi/application/kis/scheduler/KisAuthScheduler.java
+++ src/main/java/muzusi/application/kis/scheduler/KisAuthScheduler.java
@@ -1,22 +1,22 @@
 package muzusi.application.kis.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import muzusi.application.kis.service.KisOAuthService;
+import muzusi.application.kis.service.KisAuthUpdater;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class KisAuthScheduler {
-    private final KisOAuthService kisOAuthService;
+    private final KisAuthUpdater kisAuthUpdater;
     
     @Scheduled(cron = "0 0 7 * * ?")
     public void runIssueAccessTokenJob() {
-        kisOAuthService.saveAccessToken();
+        kisAuthUpdater.saveAccessToken();
     }
     
     @Scheduled(cron = "0 55 8 * * ?")
     public void runIssueWebSocketKeyJob() {
-        kisOAuthService.saveWebSocketKey();
+        kisAuthUpdater.saveWebSocketKey();
     }
 }
--- src/main/java/muzusi/application/kis/service/KisAuthUpdater.java
+++ src/main/java/muzusi/application/kis/service/KisAuthUpdater.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class KisOAuthService {
+public class KisAuthUpdater {
     private final KisOAuthClient kisOAuthClient;
     private final KisAuthService kisAuthService;
 
