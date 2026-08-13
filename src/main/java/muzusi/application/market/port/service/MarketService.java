13f98869514468c34711215f23dc3823be82aa00
hky035 <hky035@gmail.com>
2026-08-13 23:32:37 +0900

    feat: 국내 주식시장 개장 여부 확인 로직 구현
     - 국내 주식시장 포트를 통한 개장 여부 확인

---
+++ src/main/java/muzusi/application/market/port/service/MarketService.java
@@ -0,0 +1,15 @@
+package muzusi.application.market.port.service;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.application.market.port.MarketPort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MarketService {
+    private final MarketPort marketPort;
+    
+    public boolean isMarketOpen() {
+        return marketPort.isOpen();
+    }
+}
