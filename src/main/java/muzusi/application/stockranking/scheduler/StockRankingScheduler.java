e36abb495fc7cf6cde2f0756a1392b78d541da50
hky035 <hky035@gmail.com>
2026-08-15 02:27:43 +0900

    refact: 주식 순위 갱신 스케줄러 리팩토링
     - 클래스명: KisStockRankingScheduler -> StockRankingScheduler
     - 패키지: application.kis.scheduler -> application.stockranking.scheduler

--- src/main/java/muzusi/application/stockranking/scheduler/StockRankingScheduler.java
+++ src/main/java/muzusi/application/stockranking/scheduler/StockRankingScheduler.java
@@ -1,22 +1,22 @@
-package muzusi.application.kis.scheduler;
+package muzusi.application.stockranking.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import muzusi.application.kis.service.KisStockRankingUpdater;
+import muzusi.application.stockranking.service.StockRankingUpdater;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class KisStockRankingScheduler {
-    private final KisStockRankingUpdater kisStockRankingUpdater;
+public class StockRankingScheduler {
+    private final StockRankingUpdater stockRankingUpdater;
     
     @Schedules({
             @Scheduled(cron = "0 0/10 9-14 * * 1-5"),
             @Scheduled(cron = "0 0,10,20,30 15 * * 1-5")
     })
-    public void runRankingJobAt3PM() {
-        kisStockRankingUpdater.saveVolumeRank();
-        kisStockRankingUpdater.saveFallingAndRisingRank();
+    public void runUpdateStockRankingJob() {
+        stockRankingUpdater.updateVolumeRank();
+        stockRankingUpdater.updateFluctuationRank();
     }
 }
