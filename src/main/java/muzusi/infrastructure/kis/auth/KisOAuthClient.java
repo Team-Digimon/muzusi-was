954b43c434a68d0178181469da6e2e5a24c7af7c
hky035 <hky035@gmail.com>
2026-08-15 00:09:34 +0900

    refact: 한국투자증권 인증 자격증명 발급 REST API 요청 클래스 리팩토링
     - KisOAuthClient
     - 중복 코드를 줄이고, 인증 토큰 예시를 주석으로 명시

--- src/main/java/muzusi/infrastructure/kis/auth/KisOAuthClient.java
+++ src/main/java/muzusi/infrastructure/kis/auth/KisOAuthClient.java
@@ -23,40 +23,27 @@ public class KisOAuthClient {
     private final KisProperties kisProperties;
     private final ObjectMapper objectMapper;
     
+    private static final String ACCESS_TOKEN_FORMAT = "%s %s";
+    
     /**
      * 한국투자증권 접근 토큰 발급 요청 메서드
      *
      * @param appKey    한국투자증권 앱키
      * @param appSecret 한국투자증권 앱시크릿
-     * @return          한국투자증권 접근 토큰
+     * @return          한국투자증권 접근 토큰 {@code "type value"} (ex. {@code "Bearer xxx"})
      */
     public String getAccessToken(String appKey, String appSecret) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
         Map<String, String> body = new HashMap<>();
         body.put("grant_type", "client_credentials");
         body.put("appkey", appKey);
         body.put("appsecret", appSecret);
-
-        HttpEntity<Map<String, String>> requestInfo = new HttpEntity<>(body, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        try {
-            ResponseEntity<String> response = restTemplate.exchange(
-                    kisProperties.getUrl(KisUrlConstant.ACCESS_TOKEN_ISSUE),
-                    HttpMethod.POST,
-                    requestInfo,
-                    String.class
-            );
-
-            JsonNode rootNode = objectMapper.readTree(response.getBody());
-
-            return rootNode.path("token_type").asText() + " " + rootNode.path("access_token").asText();
-        } catch (Exception e) {
-            throw new KisOAuthApiException(e);
-        }
+        
+        JsonNode rootNode = requestCredential(kisProperties.getUrl(KisUrlConstant.ACCESS_TOKEN_ISSUE), body);
+        
+        String tokenType = rootNode.path("token_type").asText();
+        String tokenValue = rootNode.path("access_token").asText();
+        
+        return ACCESS_TOKEN_FORMAT.formatted(tokenType, tokenValue);
     }
     
     /**
@@ -67,13 +54,25 @@ public class KisOAuthClient {
      * @return          한국투자증권 웹소켓 접속키
      */
     public String getWebSocketKey(String appKey, String appSecret) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
         Map<String, String> body = new HashMap<>();
         body.put("grant_type", "client_credentials");
         body.put("appkey", appKey);
         body.put("secretkey", appSecret);
+        
+        JsonNode rootNode = requestCredential(kisProperties.getUrl(KisUrlConstant.WEBSOCKET_KEY_ISSUE), body);
+        
+        return rootNode.path("approval_key").asText();
+    }
+    
+    /**
+     * 한국투자증권 인증키 발급 요청 메서드
+     *
+     * @param url       요청 URL
+     * @return          응답을 {@link JsonNode}로 파싱한 결과
+     */
+    private JsonNode requestCredential(String url, Map<String, String> body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<Map<String, String>> requestInfo = new HttpEntity<>(body, headers);
 
@@ -81,15 +80,13 @@ public class KisOAuthClient {
 
         try {
             ResponseEntity<String> response = restTemplate.exchange(
-                    kisProperties.getUrl(KisUrlConstant.WEBSOCKET_KEY_ISSUE),
+                    url,
                     HttpMethod.POST,
                     requestInfo,
                     String.class
             );
 
-            JsonNode rootNode = objectMapper.readTree(response.getBody());
-
-            return rootNode.path("approval_key").asText();
+            return objectMapper.readTree(response.getBody());
         } catch (Exception e) {
             throw new KisOAuthApiException(e);
         }
