895a28b42471090057cb9237930bd8baadfc0ce3
hky035 <hky035@gmail.com>
2026-08-16 00:38:46 +0900

    feat:  주식 분봉 차트 조회 외부 인프라 연동을 위한 포트 및 어댑터 구현
     - Port: FetchStockChartPort / Adapter: KisFetchStockChartAdapter

---
+++ src/main/java/muzusi/infrastructure/kis/stockchart/adapter/KisFetchStockChartAdapter.java
@@ -0,0 +1,28 @@
+package muzusi.infrastructure.kis.stockchart.adapter;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.application.stockchart.port.FetchStockChartPort;
+import muzusi.infrastructure.kis.stockchart.client.KisStockChartClient;
+import muzusi.application.stockchart.dto.StockChartDto;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class KisFetchStockChartAdapter implements FetchStockChartPort {
+    private final KisStockChartClient stockChartClient;
+    
+    /**
+     * 한국투자증권 당일분봉조회 API를 통해 {@code gap} 간격의 분봉 데이터를 조회해 차트 정보를 반환하는 메서드
+     *
+     * @param stockCode 주식 종목 코드 번호
+     * @param time      조회 시간
+     * @param gap       차트 간격
+     * @return          차트 정보 DTO
+     */
+    @Override
+    public StockChartDto getStockMinutesChart(String stockCode, LocalDateTime time, int gap) {
+        return stockChartClient.getStockMinutesChart(stockCode, time, gap);
+    }
+}
