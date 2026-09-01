6e2841b9e85d239d5703b22cb79ba6c6fe2d11b8
hky035 <hky035@gmail.com>
2026-09-01 12:34:21 +0900

    feat: 주식 분봉 데이터 삭제 도메인 로직 구현

--- src/main/java/muzusi/domain/stockcandle/repository/StockMinuteCandleRepository.java
+++ src/main/java/muzusi/domain/stockcandle/repository/StockMinuteCandleRepository.java
@@ -3,9 +3,17 @@ package muzusi.domain.stockcandle.repository;
 import muzusi.domain.stockcandle.entity.StockMinuteCandle;
 import muzusi.domain.stockcandle.entity.StockMinuteCandleId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StockMinuteCandleRepository extends JpaRepository<StockMinuteCandle, StockMinuteCandleId>, CustomStockMinuteCandleRepository {
     List<StockMinuteCandle> findByIdStockCodeOrderByIdDateTimeAsc(String stockCode);
+    
+    @Modifying(clearAutomatically = true)
+    @Query("delete from stock_minute_candle c where c.id.dateTime < :datetime")
+    int deleteByIdDateTimeBefore(@Param(value = "datetime") LocalDateTime datetime);
 }
--- src/main/java/muzusi/domain/stockcandle/service/StockMinuteCandleService.java
+++ src/main/java/muzusi/domain/stockcandle/service/StockMinuteCandleService.java
@@ -5,6 +5,7 @@ import muzusi.domain.stockcandle.entity.StockMinuteCandle;
 import muzusi.domain.stockcandle.repository.StockMinuteCandleRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,5 +19,9 @@ public class StockMinuteCandleService {
     
     public List<StockMinuteCandle> readByStockCode(String stockCode) {
         return stockMinuteCandleRepository.findByIdStockCodeOrderByIdDateTimeAsc(stockCode);
+    }
+    
+    public int deleteByDateTimeBefore(LocalDateTime time) {
+        return stockMinuteCandleRepository.deleteByIdDateTimeBefore(time);
     }
 }
