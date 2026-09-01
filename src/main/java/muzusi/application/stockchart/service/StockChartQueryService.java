146f275a688f113e5b2a1f62bf682e7b637be765
hky035 <hky035@gmail.com>
2026-09-01 11:51:44 +0900

    feat: StockChartQueryService 내 분봉 조회 방식 통합
     - 기존 당일/일주일 분봉 조회 로직의 분리를 통합
       - 이에 따른 불필요한 메서드 정리
     - StockChartDto 내 DTO 변환 정적 팩토리 메서드 추가
     - StockPeriodType 내 분봉 관련 인스턴스 MINUTES로 통합

--- src/main/java/muzusi/application/stockchart/service/StockChartQueryService.java
+++ src/main/java/muzusi/application/stockchart/service/StockChartQueryService.java
@@ -2,52 +2,44 @@ package muzusi.application.stockchart.service;
 
 import lombok.RequiredArgsConstructor;
 import muzusi.application.stockchart.dto.StockChartDto;
-import muzusi.domain.stock.exception.StockErrorType;
 import muzusi.domain.stock.service.StockDailyService;
-import muzusi.domain.stock.service.StockMinutesService;
 import muzusi.domain.stock.service.StockMonthlyService;
 import muzusi.domain.stock.service.StockWeeklyService;
 import muzusi.domain.stock.service.StockYearlyService;
 import muzusi.domain.stock.type.StockPeriodType;
-import muzusi.global.exception.CustomException;
+import muzusi.domain.stockcandle.service.StockMinuteCandleService;
 import org.springframework.stereotype.Service;
 
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StockChartQueryService {
-    private final StockMinutesService stockMinutesService;
+    private final StockMinuteCandleService stockMinuteCandleService;
     private final StockDailyService stockDailyService;
     private final StockWeeklyService stockWeeklyService;
     private final StockMonthlyService stockMonthlyService;
     private final StockYearlyService stockYearlyService;
-    
-    private final LocalTime openTime = LocalTime.of(9, 10);
-    private final LocalTime closeTime = LocalTime.of(16, 0);
 
     /**
-     * 과거 주식 차트 불러오는 메서드
-     * StockPeriodType
-     * - MINUTES_TODAY: 당일 주식 분봉 차트 데이터를 조회
-     * - MINUTES_WEEK: 지난 1주일 간 주식 분봉 차트 데이터를 조회
-     * - DAILY: 일 단위 주식 차트 데이터를 조회
-     * - WEEKLY: 주 단위 주식 차트 데이터를 조회
-     * - MONTHLY: 월 단위 주식 차트 데이터를 조회
-     * - YEARLY: 연 단위 주식 차트 데이터를 조회
+     * 주식 차트 정보를 조회하는 메서드
+     *
+     * <p> {@link StockPeriodType}에 따라 조회 대상 차트를 다르게 설정해 반환
+     *
+     * <ul><li> MINUTES: 주식 분봉 차트 데이터를 조회</li></ul>
+     * <ul><li> DAILY: 일 단위 주식 차트 데이터를 조회</li></ul>
+     * <ul><li> WEEKLY: 주 단위 주식 차트 데이터를 조회</li></ul>
+     * <ul><li> MONTHLY: 월 단위 주식 차트 데이터를 조회</li></ul>
+     * <ul><li> YEARLY: 연 단위 주식 차트 데이터를 조회</li></ul>
      *
      * @param stockCode : 주식 코드
-     * @param stockPeriodType : 주식 차트 기간 유형 (MINUTES_TODAY, MINUTES_WEEK, DAILY, WEEKLY, MONTHLY, YEARLY)
-     * @return 과거 주식 차트
+     * @param stockPeriodType : 주식 차트 기간 유형 {@link StockPeriodType}
+     * @return 주식 차트 목록
      */
     public List<StockChartDto> getStockHistoryByType(String stockCode, StockPeriodType stockPeriodType) {
         return switch (stockPeriodType) {
-            case MINUTES_TODAY -> getMinutesTodayChartByStockCode(stockCode);
-            case MINUTES_WEEK -> getMinutesWeekChartByStockCode(stockCode);
+            case MINUTES -> stockMinuteCandleService.readByStockCode(stockCode)
+                    .stream().map(StockChartDto::from).toList();
             case DAILY -> stockDailyService.readByStockCode(stockCode)
                     .stream().map(StockChartDto::from).toList();
             case WEEKLY -> stockWeeklyService.readByStockCode(stockCode)
@@ -57,35 +49,5 @@ public class StockChartQueryService {
             case YEARLY -> stockYearlyService.readByStockCode(stockCode)
                     .stream().map(StockChartDto::from).toList();
         };
-    }
-
-    /**
-     * 당일 주식 분봉 차트를 반환하는 메서드
-     *
-     * @param stockCode : 주식 코드
-     * @return 당일 주식 분봉 차트
-     */
-    private List<StockChartDto> getMinutesTodayChartByStockCode(String stockCode) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalTime time = now.toLocalTime();
-        boolean isWeekend = now.getDayOfWeek() == DayOfWeek.SATURDAY || now.getDayOfWeek() == DayOfWeek.SUNDAY;
-        
-        if (time.isBefore(openTime) || time.isAfter(closeTime) || isWeekend) {
-            throw new CustomException(StockErrorType.NOT_AVAILABLE_MINUTES_CHART);
-        }
-        
-        return stockMinutesService.readAllInCache(stockCode);
-    }
-
-    /**
-     * 지난 1주일 간 주식 분봉 차트를 반환하는 메서드
-     *
-     * @param stockCode : 주식 코드
-     * @return 지난 1주일 간 주식 분봉 차트
-     */
-    private List<StockChartDto> getMinutesWeekChartByStockCode(String stockCode) {
-        return stockMinutesService.readByStockCode(stockCode).stream()
-                .flatMap(stockMinutes -> stockMinutes.getMinutesChart().stream())
-                .collect(Collectors.toList());
     }
 }
