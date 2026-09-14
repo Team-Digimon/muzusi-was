b90f97ef215073637e9f8bc725a1a2d38727d4c5
hky035 <hky035@gmail.com>
2026-09-15 01:30:44 +0900

    docs: KisErrorParser 주석 작성

--- src/main/java/muzusi/infrastructure/kis/util/KisErrorParser.java
+++ src/main/java/muzusi/infrastructure/kis/util/KisErrorParser.java
@@ -7,9 +7,16 @@ import muzusi.infrastructure.kis.exception.KisApiRateLimitExceedException;
 public final class KisErrorParser {
     private static final String ERROR_RT_CD = "1";
     private static final String API_RATE_LIMIT_EXCEED_ERROR_MSG_CD = "EGW00201";
-    
+
     private KisErrorParser() { }
-    
+
+    /**
+     * 응답의 rt_cd가 에러를 나타내면 msg_cd에 따라 적절한 예외를 던진다.
+     *
+     * @param response                          한국투자증권 REST API 응답
+     * @throws KisApiRateLimitExceedException   유량 초과 에러인 경우 (msg_cd: {@value API_RATE_LIMIT_EXCEED_ERROR_MSG_CD})
+     * @throws KisApiException                  response가 null이거나, 그 외 에러 응답인 경우
+     */
     public static void validate(KisResponse response) {
         if (response == null) {
             throw new KisApiException("한국투자증권 REST API 응답 본문이 비어있습니다.");
