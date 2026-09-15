26a663e8965734be342bbe140a04730b9b3493ad
hky035 <hky035@gmail.com>
2026-09-15 19:03:11 +0900

    feat: `@KisRateLimit`와 `@Retryable` 동시 사용으로 인한 프록시 실행 순서 정의

--- src/main/java/muzusi/infrastructure/kis/aop/KisRateLimitAspect.java
+++ src/main/java/muzusi/infrastructure/kis/aop/KisRateLimitAspect.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 @RequiredArgsConstructor
 public class KisRateLimitAspect {
     private final RateLimiter kisRateLimiter;
