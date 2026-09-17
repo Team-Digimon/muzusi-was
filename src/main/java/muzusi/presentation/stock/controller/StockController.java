5b52d7a3da2f341712577a5b7541dc609773f206
hky035 <hky035@gmail.com>
2026-09-17 14:42:45 +0900

    feat: 주식 기본 정보 조회 API 구현

---
+++ src/main/java/muzusi/presentation/stock/controller/StockController.java
@@ -0,0 +1,22 @@
+package muzusi.presentation.stock.controller;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.application.stock.service.StockQueryService;
+import muzusi.global.response.success.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stocks")
+@RequiredArgsConstructor
+public class StockController {
+    private final StockQueryService stockQueryService;
+
+    @GetMapping("/{stockCode}")
+    public ResponseEntity<?> getStock(@PathVariable(name = "stockCode") String stockCode) {
+        return ResponseEntity.ok(SuccessResponse.from(stockQueryService.getStock(stockCode)));
+    }
+}
