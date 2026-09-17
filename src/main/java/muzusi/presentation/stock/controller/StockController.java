3050cccde365629e637c37bdfcf5c8a3d72076d7
hky035 <hky035@gmail.com>
2026-09-17 14:43:18 +0900

    docs: 주식 기본 정보 조회 API Swagger 문서 작성

--- src/main/java/muzusi/presentation/stock/controller/StockController.java
+++ src/main/java/muzusi/presentation/stock/controller/StockController.java
@@ -3,6 +3,7 @@ package muzusi.presentation.stock.controller;
 import lombok.RequiredArgsConstructor;
 import muzusi.application.stock.service.StockQueryService;
 import muzusi.global.response.success.SuccessResponse;
+import muzusi.presentation.stock.api.StockApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stocks")
 @RequiredArgsConstructor
-public class StockController {
+public class StockController implements StockApi {
     private final StockQueryService stockQueryService;
 
+    @Override
     @GetMapping("/{stockCode}")
     public ResponseEntity<?> getStock(@PathVariable(name = "stockCode") String stockCode) {
         return ResponseEntity.ok(SuccessResponse.from(stockQueryService.getStock(stockCode)));
