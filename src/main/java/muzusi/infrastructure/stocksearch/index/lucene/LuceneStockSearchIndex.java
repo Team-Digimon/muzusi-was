3a3a3641b38485268877c98b17e847b3d38d446c
hky035 <hky035@gmail.com>
2026-09-18 21:03:43 +0900

    chore: StockSearchIndex 네이밍변경

--- src/main/java/muzusi/infrastructure/stocksearch/index/lucene/LuceneStockSearchIndex.java
+++ src/main/java/muzusi/infrastructure/stocksearch/index/lucene/LuceneStockSearchIndex.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.util.List;
 
 @Component
-public class StockSearchIndex {
+public class LuceneStockSearchIndex {
     private IndexSearcher indexSearcher;
     
     /**
