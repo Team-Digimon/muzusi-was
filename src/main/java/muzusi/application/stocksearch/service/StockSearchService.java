1d695fddc7ed0c7506dcde6489eb099ea2900575
hky035 <hky035@gmail.com>
2026-09-19 12:04:20 +0900

    feat: 주식 검색 로직을 Lucene 기반 검색으로 변경
      - 기존 MongoDB 방식 -> Lucene 기반 검색 로직 적용

--- src/main/java/muzusi/application/stocksearch/service/StockSearchService.java
+++ src/main/java/muzusi/application/stocksearch/service/StockSearchService.java
@@ -1,29 +1,67 @@
 package muzusi.application.stocksearch.service;
 
 import lombok.RequiredArgsConstructor;
-import muzusi.application.stocksearch.dto.StockItemDto;
+import muzusi.application.stocksearch.dto.StockSearchCandidate;
+import muzusi.application.stocksearch.dto.StockSearchResponse;
+import muzusi.application.stocksearch.port.StockSearchPort;
+import muzusi.application.stocksearch.util.StockSearchRankingScoreCalculator;
 import muzusi.domain.stock.service.StockItemService;
+import muzusi.domain.stocksearch.entity.StockSearchStat;
+import muzusi.domain.stocksearch.service.StockSearchStatService;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StockSearchService {
     private final StockItemService stockItemService;
-
+    private final StockSearchPort stockSearchPort;
+    private final StockSearchStatService stockSearchStatService;
+    
+    private static final int SEARCH_RESULT_LIMIT = 10;
+    
     /**
      * 검색어 자동완성을 위한 주식 검색 메서드
      *
      * @param keyword : 사용자 입력한 값
      * @return : 자동완성 리스트
      */
-    public List<StockItemDto> searchStocks(String keyword) {
-        return stockItemService.readByKeyword(keyword)
-                .stream().map(StockItemDto::fromEntity)
+    public List<StockSearchResponse> searchStocks(String keyword) {
+        List<StockSearchCandidate> candidates = stockSearchPort.search(keyword, SEARCH_RESULT_LIMIT);
+        
+        Map<String, Integer> stockSearchStatMap = getStockSearchCountMap(candidates);
+        
+        return candidates.stream()
+                .map(candidate -> {
+                    int searchCount = stockSearchStatMap.getOrDefault(candidate.stockCode(), 0);
+                    
+                    double finalScore = StockSearchRankingScoreCalculator.calculateFinalScore(candidate.score(), searchCount);
+                    return candidate.withScore(finalScore);
+                })
+                .sorted(Comparator.comparingDouble(StockSearchCandidate::score).reversed())
+                .map(StockSearchResponse::from)
                 .toList();
     }
-
+    
+    /**
+     * 주어진 검색 결과 후보에 해당하는 검색 통계 정보를 반환하는 메서드
+     *
+     * @param candidates    검색 결과 후보
+     * @return              각 종목 별 검색 통계 Map
+     */
+    private Map<String, Integer> getStockSearchCountMap(List<StockSearchCandidate> candidates) {
+        List<String> stockCodes = candidates.stream().map(StockSearchCandidate::stockCode).toList();
+        List<StockSearchStat> stockSearchStats = stockSearchStatService.readByStockCodeIn(stockCodes);
+        
+        return stockSearchStats.stream().collect(
+                Collectors.toMap(StockSearchStat::getStockCode, StockSearchStat::getSearchCount)
+        );
+    }
+    
     /**
      * 종목 검색 횟수를 증가시키는 메서드
      *
