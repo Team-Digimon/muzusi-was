8d91780eb46b6cb2818a55da1c9ce633d9c85c9e
hky035 <hky035@gmail.com>
2026-09-19 18:08:33 +0900

    feat: 검색 빈도 증가 도메인 로직 구현

--- src/main/java/muzusi/domain/stocksearch/repository/StockSearchStatRepository.java
+++ src/main/java/muzusi/domain/stocksearch/repository/StockSearchStatRepository.java
@@ -2,9 +2,16 @@ package muzusi.domain.stocksearch.repository;
 
 import muzusi.domain.stocksearch.entity.StockSearchStat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StockSearchStatRepository extends JpaRepository<StockSearchStat, Long> {
     List<StockSearchStat> findByStockCodeIn(List<String> stockCodes);
+    
+    @Modifying
+    @Query("UPDATE StockSearchStat s SET s.searchCount = s.searchCount + 1 WHERE s.stockCode = :stockCode")
+    int increaseSearchCount(@Param(value = "stockCode") String stockCode);
 }
