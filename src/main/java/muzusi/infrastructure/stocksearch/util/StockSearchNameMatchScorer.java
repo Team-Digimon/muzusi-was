7d0f9888dbf8a4da6e93a6b429ddcec9dec77f0c
hky035 <hky035@gmail.com>
2026-09-19 12:42:44 +0900

    feat: 주식 검색 결과 점수 계산 유틸 클래스 내 StockSearchMatchType 적용

--- src/main/java/muzusi/infrastructure/stocksearch/util/StockSearchNameMatchScorer.java
+++ src/main/java/muzusi/infrastructure/stocksearch/util/StockSearchNameMatchScorer.java
@@ -1,17 +1,15 @@
 package muzusi.infrastructure.stocksearch.util;
 
+import muzusi.application.stocksearch.policy.StockSearchMatchType;
+
 public class StockSearchNameMatchScorer {
-    private static final int ALL_MATCH_SCORE = 100;
-    private static final int START_MATCH_SCORE = 50;
-    private static final int DEFAULT_MATCH_SCORE = 30;
-    
     /**
      * 검색 결과 주식 이름({@code stockName})과 검색 키워드({@code keyword}) 간 유사도 점수를 반환하는 메서드
      *
      * <ul>
-     *     <li>완전히 동일한 경우, {@value ALL_MATCH_SCORE}를 반환</li>
-     *     <li>키워드가 결과의 접두어인 경우, {@value START_MATCH_SCORE}를 반환</li>
-     *     <li>그 외의 경우, {@value DEFAULT_MATCH_SCORE}를 반환</li>
+     *     <li>완전히 동일한 경우, {@link StockSearchMatchType#EXACT}를 반환</li>
+     *     <li>키워드가 결과의 접두어인 경우, {@link StockSearchMatchType#PREFIX}를 반환</li>
+     *     <li>그 외의 경우, {@link StockSearchMatchType#DEFAULT}를 반환</li>
      * </ul>
      *
      * @param stockName 검색 결과 주식 이름
@@ -20,13 +18,13 @@ public class StockSearchNameMatchScorer {
      */
     public static double matchScore(String stockName, String keyword) {
         if (stockName.equals(keyword)) {
-            return ALL_MATCH_SCORE;
+            return StockSearchMatchType.EXACT.getScore();
         }
         
         if (stockName.startsWith(keyword)) {
-            return START_MATCH_SCORE;
+            return StockSearchMatchType.PREFIX.getScore();
         }
         
-        return DEFAULT_MATCH_SCORE;
+        return StockSearchMatchType.DEFAULT.getScore();
     }
 }
