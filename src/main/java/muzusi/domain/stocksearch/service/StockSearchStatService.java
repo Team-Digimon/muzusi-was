f788f8dfafdef0f805f516bb20c6e8eb8e020f88
hky035 <hky035@gmail.com>
2026-09-19 12:00:49 +0900

    feat: 주식 검색 통계 조회 도메인 로직 구현

---
+++ src/main/java/muzusi/domain/stocksearch/service/StockSearchStatService.java
@@ -0,0 +1,18 @@
+package muzusi.domain.stocksearch.service;
+
+import lombok.RequiredArgsConstructor;
+import muzusi.domain.stocksearch.entity.StockSearchStat;
+import muzusi.domain.stocksearch.repository.StockSearchStatRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StockSearchStatService {
+    private final StockSearchStatRepository stockSearchStatRepository;
+    
+    public List<StockSearchStat> readByStockCodeIn(List<String> stockCodes) {
+        return stockSearchStatRepository.findByStockCodeIn(stockCodes);
+    }
+}
